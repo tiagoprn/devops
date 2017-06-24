a91db80664489645ba9b77c8776edcf953f45b07
tiago.paranhos <tiagoprn@gmail.com>
2017-06-23 21:33:33 -0300

    typo

--- ansible_playbooks/files/run_portainer.sh
+++ ansible_playbooks/files/run_portainer.sh
@@ -5,6 +5,6 @@ printf 'Encrypting the generated admin passwd...\n'
 docker run --rm httpd:2.4-alpine htpasswd -nbB admin $(cat /root/portainer.admin.passwd) | cut -d ":" -f 2 > /root/portainer.admin.encrypted
 printf 'Done. The portainer admin passwd is at "/root/portainer.admin.passwd".\n'
 printf 'Starting portainer in the background...\n'
-docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer --admin-password $(cat /root/portainer.admin.encrypted) & 
+docker run -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer --admin-password $(cat /root/portainer.admin.encrypted) 
 printf 'Portainer started. It can be reached through port 9000.'
 
