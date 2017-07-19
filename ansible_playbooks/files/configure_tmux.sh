e361f1a6491f067b26c799190a87e71926910959
tiago.paranhos <tiagoprn@gmail.com>
2017-07-18 23:44:54 -0300

    Configure tmux script will no more try to load tmux.

--- ansible_playbooks/files/configure_tmux.sh
+++ ansible_playbooks/files/configure_tmux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-source ~/.bashrc
 if [ ! -d ~/.tmux ]; then 
     mkdir ~/.tmux
     ln -s ~/dot_files/.tmux.conf ~/.tmux.conf
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-    tmux && tmux source-file ~/.tmux.conf
 fi
