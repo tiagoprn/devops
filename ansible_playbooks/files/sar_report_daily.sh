41404c7d89da01ec4312926042196de5b75250a2
tiago.paranhos <tiagoprn@gmail.com>
2017-07-31 16:37:30 -0300

    Final changes to sysstat logging functionality.

--- ansible_playbooks/files/sar_report_daily.sh
+++ ansible_playbooks/files/sar_report_daily.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DATE=$(date +%d)
 FULLDATE=$(date +%Y%m%d)
-mkdir -p /var/log/sysstat/reports
-sadf -T -jh /var/log/sa/sa$DATE -- | sed 's/;/,/g' > /var/log/sysstat/reports/system_stats_$FULLDATE.json
+REPORTS_DIR=/var/log/syslog-ng/systat_daily_reports
+mkdir -p $REPORTS_DIR 
+sadf -T -jh /var/log/sa/sa$DATE -- -n DEV -q -d -r -u | sed 's/;/,/g' > $REPORTS_DIR/system_stats_$FULLDATE.json
