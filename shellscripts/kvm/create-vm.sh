281256f9245f13a5df7f9a5a476e85dc94bc5e4c
tiago.paranhos <tiagoprn@gmail.com>
2017-12-20 16:14:47 -0200

    Enhancing the create-vm script.

--- shellscripts/kvm/create-vm.sh
+++ shellscripts/kvm/create-vm.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 # e.g.
-# ./create-vm.sh -n centos7-02 -b br1 -r 1024 -c 1
+SYNTAX="./create-vm.sh -n [VM_NAME] -b [BRIDGE_NAME] -r [RAM_MB] -c [NUMBER_OF_CPUS]"
+HELP="./create-vm.sh -n centos7-02 -b br1 -r 1024 -c 1"
 
 while getopts n:b:r:c: option
 do
@@ -14,7 +15,26 @@ do
   esac
 done
 
-# TODO: set values if any of the variables above was not set
+echo 'Parsing script parameters...'
+if [[ -z $VM_NAME ]]; then
+    printf "Missing -n parameter. Syntax: \n\n$SYNTAX\n\nE.g.: \n\n$HELP\n";
+    exit 1
+fi
+
+if [[ -z $BRIDGE_NAME ]]; then
+    printf "Missing -b parameter. Syntax: \n\n$SYNTAX\n\nE.g.: \n\n$HELP\n";
+    exit 1
+fi
+
+if [[ -z $RAM_MB ]]; then
+    printf "Missing -r parameter. I will use a default value.\n";
+    RAM_MB=1024
+fi
+
+if [[ -z $CPUS ]]; then
+    printf "Missing -c parameter. I will use a default value.\n";
+    CPUS=1
+fi
 
 echo 'Downloading the CentOS 7 cloud image...';
 bash ./download_centos7_cloud_image.sh;
@@ -38,5 +58,6 @@ sudo virt-install --import --name $VM_NAME --ram $RAM_MB --vcpus $CPUS --disk /k
 echo 'Ejecting the iso created by cloud-init...'
 sudo virsh change-media $VM_NAME hda --eject --config 
 
-echo 'FINISHED! Here are the credentials:'
+echo 'FINISHED! The machine is named' $VM_NAME', has a network bridge at '$BRIDGE_NAME', '$RAM_MB' MB of RAM and uses '$CPUS 'cpu(s). Here are its credentials:'
 bash ./get_domain_ip.sh $VM_NAME
+
