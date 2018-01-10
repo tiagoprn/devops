8a3340a30c5a9f8e6be5b6889e11b0c66c02c34a
tiago.paranhos <tiagoprn@gmail.com>
2018-01-10 09:03:33 -0200

    Removing the old monolith.

--- ansible_playbooks/provision-centos7/roles/tasks/files/configure_tmux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -d ~/.tmux ]; then 
-    mkdir ~/.tmux
-    ln -s ~/dot_files/.tmux.conf ~/.tmux.conf
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
--- ansible_playbooks/provision-centos7/roles/tasks/files/configure_vim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -fr ~/.vim
-rm -fr ~/.vimrc 
-ln -s ~/dot_files/.vimrc ~/.vimrc
-ln -s ~/dot_files/.vim ~/.vim
-mkdir ~/.vim/bundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
