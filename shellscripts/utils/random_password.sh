75d94e2d139d51d9ce8d40fa7e47f132fa3f03f0
tiago.paranhos <tiagoprn@gmail.com>
2018-01-17 22:28:25 -0200

    Adding a shell script to generate a random password.

---
+++ shellscripts/utils/random_password.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Generates a random password containing uppercase and lowercase characters and digits, with the password length random varying from 10 to 20 characters 
+python -c 'import os; import random; import string; print("".join(random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(random.randint(10, 20))))'
+
