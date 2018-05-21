40f6a2557e3edc00afd622781714de549f9e77db
tiago.paranhos <tiagoprn@gmail.com>
2018-05-21 08:04:48 -0300

    Updating postgres container to its final version.

--- docker_composes/postgres/utils/setup.sh
+++ docker_composes/postgres/utils/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 5;  # wait for the container to start to run the commands
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER ROLE $POSTGRES_USER SET client_encoding TO 'utf8';"
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER ROLE $POSTGRES_USER SET default_transaction_isolation TO 'read committed';"
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c "ALTER ROLE $POSTGRES_USER SET timezone TO 'UTC';"
