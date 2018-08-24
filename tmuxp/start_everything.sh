5ca574f093eec4b95f3efe477dfeb99aea4711b4
tiago.paranhos <tiagoprn@gmail.com>
2018-08-24 07:53:23 -0300

    adding tmuxp session.

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 tmuxp load -d /storage/src/devops/tmuxp/monitoring.yml
+tmuxp load -d /storage/src/devops/tmuxp/logs.yml
 tmuxp load -d /storage/src/devops/tmuxp/devops.yml
 tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
