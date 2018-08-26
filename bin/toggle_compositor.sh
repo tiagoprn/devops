bc6def9e03b631b36af2725831befd5e6f654a37
tiago.paranhos <tiagoprn@gmail.com>
2018-08-26 19:23:24 -0300

    new script

--- bin/toggle_compositor.sh
+++ bin/toggle_compositor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid=$(ps aux | grep [c]ompton | awk '{print $2}')
+pid=$(ps aux | grep [c]ompton | awk '{print $2}')  # the [c] on the beginning of the program name assures it does not include the own grep pid on the output
 if [ -z "$pid" ]
 then
     echo 'Compton not running, will run it.';
