ff6862cf2618ecb54499c86e739e98987f5c0d08
tiago.paranhos <tiagoprn@gmail.com>
2018-10-01 07:23:44 -0300

    Updated

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-tmuxp load -d /storage/src/devops/tmuxp/monitoring.yml
 tmuxp load -d /storage/src/devops/tmuxp/logs.yml
 tmuxp load -d /storage/src/devops/tmuxp/devops.yml
 tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
