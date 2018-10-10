ce244d6b27b8c593783949ec43bb10cef5d257a6
tiago.paranhos <tiagoprn@gmail.com>
2018-10-10 10:45:30 -0300

    Updated the tmux sessions on tmuxp.

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 tmuxp load -d /storage/src/devops/tmuxp/logs.yml
-tmuxp load -d /storage/src/devops/tmuxp/devops.yml
+tmuxp load -d /storage/src/devops/tmuxp/ansible.yml
+tmuxp load -d /storage/src/devops/tmuxp/kvm.yml
 tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
 tmuxp load -d /storage/src/devops/tmuxp/home.yml
 tmuxp load -d /storage/src/devops/tmuxp/dev.yml
