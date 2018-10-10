a424661fb51b19277e1caf646dd5340cc88d1c26
tiago.paranhos <tiagoprn@gmail.com>
2018-10-10 07:52:49 -0300

    New tmux session for development.

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -3,3 +3,4 @@ tmuxp load -d /storage/src/devops/tmuxp/logs.yml
 tmuxp load -d /storage/src/devops/tmuxp/devops.yml
 tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
 tmuxp load -d /storage/src/devops/tmuxp/home.yml
+tmuxp load -d /storage/src/devops/tmuxp/dev.yml
