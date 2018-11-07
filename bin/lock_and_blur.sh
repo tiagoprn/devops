1a3132446f647955fc1e2242616066598afb248c
tiago.paranhos <tiagoprn@gmail.com>
2018-11-07 08:49:14 -0200

    Changing i3lock configuration.

--- bin/lock_and_blur.sh
+++ bin/lock_and_blur.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-lighticon="/storage/src/devops/icons/lock-icon-light.png"
-darkicon="/storage/src/devops/icons/lock-icon-dark.png"
 tmpbg='/tmp/screen.png'
 
 # take a screenshot
@@ -18,16 +16,14 @@ COLOR=$(convert "$tmpbg" -gravity center -crop 100x100+0+0 +repage -colorspace h
     -resize 1x1 txt:- | awk -F '[%$]' 'NR==2{gsub(",",""); printf "%.0f\n", $(NF-1)}');
 
 # change the color ring colors to leave the middle of the feedback ring
-# transparent and the outside to use either dark or light colors based on the 
+# transparent and the outside to use either dark or light colors based on the
 # screenshot
 if [ "$COLOR" -gt "$VALUE" ]; then #light background, use dark icon
-    icon="$darkicon"
     PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
         --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
         --separatorcolor=22222260 --insidevercolor=ffffff1c \
         --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c)
 else # dark background so use the light icon
-    icon="$lighticon"
     PARAM=(--insidecolor=ffffff00 --ringcolor=ffffff3e \
         --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 \
         --separatorcolor=22222260 --insidevercolor=0000001c \
@@ -37,8 +33,5 @@ fi
 # blur the screenshot by resizing and scaling back up
 convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
 
-# overlay the icon onto the screenshot
-convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
-
 # lock the screen with the color parameters
-i3lock --timesize=100 -k "${PARAM[@]}" -i "$tmpbg"
+i3lock -e -f -t -n -u --timesize=100 -k "${PARAM[@]}" -i "$tmpbg" --datestr="%A, %d %b %Y"
