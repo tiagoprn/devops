711b8c894447e1d4ab18219632c0ce5457e0585a
tiago.paranhos <tiagoprn@gmail.com>
2018-11-17 22:48:10 -0200

    Final customization of the lock screen

--- bin/lock_with_comic.sh
+++ bin/lock_with_comic.sh
@@ -13,4 +13,4 @@ COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1)
 
 notify-send --urgency critical "Locking screen with comic $COMIC_PATH ..."
 
-i3lock -e -f -n -k --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y"
+i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950"
