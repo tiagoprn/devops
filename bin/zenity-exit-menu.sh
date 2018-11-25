bbca2fcc3c774f9fbf3626a507e6c9552a46f167
tiago.paranhos <tiagoprn@gmail.com>
2018-11-25 17:40:51 -0200

    Updated

--- bin/zenity-exit-menu.sh
+++ bin/zenity-exit-menu.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 while true; do
+
   choice=$(zenity  --list  --text "Leaving so soon?"\
     --list --column "Action:"\
     --title="Leaving how? " \
+    Logoff\
     Shutdown\
-    Restart\
-    Logoff);
+    Restart);
 
   case "${choice}" in
 
- "Shutdown" )
-#!/bin/bash
+    "Shutdown" )
+    #!/bin/bash
     zenity --question --text "Poweroff now?" echo $?
     case $? in
         0)sudo /sbin/shutdown -h now;;
@@ -19,26 +20,29 @@ while true; do
     esac
     ;;
 
- "Restart" )
-#!/bin/bash
+    "Restart" )
+    #!/bin/bash
     zenity --question --text "Restart now?" echo $?
     case $? in
-        0)sudo /sbin/shutdown -r now;;
-        1) exit;;
+      0)sudo /sbin/shutdown -r now;;
+      1) exit;;
     esac
     ;;
 
- "Logoff" )
-#!/bin/bash
+    "Logoff" )
+    #!/bin/bash
     zenity --question --text "Quit i3 and return to gdm?" echo $?
     case $? in
-        0) i3-msg exit;;
-        1) exit;;
+      0) i3-msg exit;;
+      1) exit;;
     esac
     ;;
 
- *)
+    *)
+
     break
     ;;
-    esac
+
+  esac
+
 done
