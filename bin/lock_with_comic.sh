be08fa4f03cb4827f0529f51a11ae50c4a2ef0d2
tiago.paranhos <tiagoprn@gmail.com>
2018-11-26 23:15:15 -0200

    reverting back

--- bin/lock_with_comic.sh
+++ bin/lock_with_comic.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
-LOCKED=$(pgrep i3lock)
+bash /storage/src/devops/bin/download_random_commitstrip.sh;
 
-if [ -z "$LOCKED" ]; then
-    # If there is not an already running i3lock
+COMIC_PATH=$HOME/tmp;
 
-    bash /storage/src/devops/bin/download_random_commitstrip.sh;
+PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
+    --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
+    --separatorcolor=22222260 --insidevercolor=ffffff1c \
+    --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c);
 
-    COMIC_PATH=$HOME/tmp;
+COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1);
 
-    PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
-        --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
-        --separatorcolor=22222260 --insidevercolor=ffffff1c \
-        --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c);
+notify-send --urgency critical "Locking screen with comic $COMIC_PATH ...";
 
-    COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1);
-
-    notify-send --urgency critical "Locking screen with comic $COMIC_PATH ...";
-
-    i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950";
-fi
+i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950";
