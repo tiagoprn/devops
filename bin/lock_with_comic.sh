6b65618190330958e918200b58f4527ee6459820
tiago.paranhos <tiagoprn@gmail.com>
2018-11-26 23:03:45 -0200

    Updated locker script to do not lock if already locked.

--- bin/lock_with_comic.sh
+++ bin/lock_with_comic.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-bash /storage/src/devops/bin/download_random_commitstrip.sh
+LOCKED=$(pgrep i3lock)
 
-COMIC_PATH=$HOME/tmp
+if [ -z "$LOCKED" ]; then
+    # If there is not an already running i3lock
 
-PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
-    --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
-    --separatorcolor=22222260 --insidevercolor=ffffff1c \
-    --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c)
+    bash /storage/src/devops/bin/download_random_commitstrip.sh;
 
-COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1)
+    COMIC_PATH=$HOME/tmp;
 
-notify-send --urgency critical "Locking screen with comic $COMIC_PATH ..."
+    PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
+        --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
+        --separatorcolor=22222260 --insidevercolor=ffffff1c \
+        --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c);
 
-i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950"
+    COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1);
+
+    notify-send --urgency critical "Locking screen with comic $COMIC_PATH ...";
+
+    i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950";
+fi
