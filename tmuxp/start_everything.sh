a93130124b13b12978485b5d6a21d90ac00f0636
tiago.paranhos <tiagoprn@gmail.com>
2019-01-05 19:58:07 -0200

    updated tmux sessions with the new virtualenvs

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 tmuxp load -d /storage/src/devops/tmuxp/logs.yml
 tmuxp load -d /storage/src/devops/tmuxp/ansible.yml
-tmuxp load -d /storage/src/devops/tmuxp/kvm.yml
 tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
 tmuxp load -d /storage/src/devops/tmuxp/home.yml
 tmuxp load -d /storage/src/devops/tmuxp/dev.yml
-tmuxp load -d /storage/src/devops/tmuxp/utils.yml
+tmuxp load -d /storage/src/devops/tmuxp/core-utils.yml
+tmuxp load -d /storage/src/devops/tmuxp/db-utils.yml
