5803e110d2e5c72948b624c234caaa587d15bc57
tiago.paranhos <tiagoprn@gmail.com>
2019-01-08 18:51:03 -0200

    Updated script to search by file contents.

--- bin/search_posts_and_trigger_vim_by_file_contents.sh
+++ bin/search_posts_and_trigger_vim_by_file_contents.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-# TODO: copy my ~/.bashrc function vim-fzf-search here
-cd /storage/src/tiagoprnl/content/posts && vim-fzf-search
+vim-fzf-search() {
+    if [ $# == 0 ]; then
+        echo 'Error: search term was not provided.'
+        return
+    fi
+    local match=$(
+      rg --color=never --line-number "$1" |
+        fzf -i --exact --no-multi --delimiter : \
+            --preview "bat --color=always --line-range {2}: {1}"
+      )
+    local file=$(echo "$match" | cut -d':' -f1)
+    if [ -n "$file" ]; then
+        $EDITOR $file +$(echo "$match" | cut -d':' -f2)
+    fi
+}
+
+contents=$(printf "i3\narch\npycharm\nnmcli" | dmenu -p 'Type the word you want to search on the folder:')
+
+cd /storage/src/tiagoprnl/content/posts && vim-fzf-search $contents
 
