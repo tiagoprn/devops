28d21ef0a6761c0caaa55b226eb800c87e290b1f
tiago.paranhos <tiagoprn@gmail.com>
2019-01-27 17:17:43 -0200

    Added docker installation role.

--- ansible_playbooks/ubuntu/18.04/initial-setup/roles/docker/files/start.sh
+++ ansible_playbooks/ubuntu/18.04/initial-setup/roles/docker/files/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PORTAINER=$(docker ps | grep portainer | awk '{print $1}')
-if [ $PORTAINER  ] 
+if [ $PORTAINER  ]
 then
     printf 'Portainer is already running on port 9000, nothing to be done here.'
-else    
+else
     printf 'Starting portainer in the background...\n'
-    cd /opt/containers/portainer && docker-compose up -d
-    printf 'Portainer started. It can be reached through port 9000.'
+    cd /opt/containers/portainer && docker-compose up -d && printf 'Portainer started. It can be reached through port 9000.'
 fi
+
