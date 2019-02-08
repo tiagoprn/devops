d0b48f9f0703c5ddbaeb7117b2ae01ed3cbfe6e2
tiago.paranhos <tiagoprn@gmail.com>
2019-02-08 19:23:11 -0200

    Updated script to run tmux

--- shellscripts/installers/install_tmux_on_home_folder.sh
+++ shellscripts/installers/install_tmux_on_home_folder.sh
@@ -8,6 +8,8 @@
 set -e
 
 TMUX_VERSION=2.8
+LIBEVENT_VERSION=2.1.8
+NCURSES_VERSION=6.1
 
 # create our directories
 mkdir -p $HOME/local $HOME/tmux_tmp
@@ -16,16 +18,19 @@ cd $HOME/tmux_tmp
 # download source files for tmux, libevent, and ncurses
 # wget -O tmux-${TMUX_VERSION}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz/download
 wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
-wget https://github.com/downloads/libevent/libevent/libevent-2.0.19-stable.tar.gz
-wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz
+
+# wget https://github.com/downloads/libevent/libevent/libevent-${LIBEVENT_VERSION}-stable.tar.gz
+wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
+
+wget ftp://ftp.gnu.org/gnu/ncurses/ncurses-${NCURSES_VERSION}.tar.gz
 
 # extract files, configure, and compile
 
 ############
 # libevent #
 ############
-tar xvzf libevent-2.0.19-stable.tar.gz
-cd libevent-2.0.19-stable
+tar xvzf libevent-$LIBEVENT_VERSION-stable.tar.gz
+cd libevent-$LIBEVENT_VERSION-stable
 ./configure --prefix=$HOME/local --disable-shared
 make
 make install
@@ -34,8 +39,8 @@ cd ..
 ############
 # ncurses  #
 ############
-tar xvzf ncurses-5.9.tar.gz
-cd ncurses-5.9
+tar xvzf ncurses-$NCURSES_VERSION.tar.gz
+cd ncurses-$NCURSES_VERSION
 ./configure --prefix=$HOME/local
 make
 make install
