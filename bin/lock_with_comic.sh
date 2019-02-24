2607543f9946e946eda5e88db9e7bec44b3fcaf3
tiago.paranhos <tiagoprn@gmail.com>
2019-02-24 12:04:11 -0300

    Fixed bug that did not trigger the lock in some cases.

--- bin/lock_with_comic.sh
+++ bin/lock_with_comic.sh
@@ -2,6 +2,8 @@
 
 bash /storage/src/devops/bin/download_random_commitstrip.sh;
 
+LOCK_DELAY=2
+
 COMIC_PATH=$HOME/tmp;
 
 PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
@@ -9,8 +11,8 @@ PARAM=(--insidecolor=00000000 --ringcolor=0000003e \
     --separatorcolor=22222260 --insidevercolor=ffffff1c \
     --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c);
 
-COMIC_PATH=$(find $COMIC_PATH -name *.jpg | sort -r | head -n 1);
+COMIC_PATH=$(find $COMIC_PATH -name '*.jpg' | sort -r | head -n 1);
 
-notify-send --urgency critical "Locking screen with comic $COMIC_PATH ...";
+notify-send --urgency critical "Screen will be locked in $LOCK_DELAY seconds with comic $COMIC_PATH ...";
 
-i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950";
+sleep $LOCK_DELAY && i3lock -e -f -n -k --datesize=30 --timesize=150 -k "${PARAM[@]}" -i $COMIC_PATH --datestr="%A, %d %b %Y" --datepos="1700:1000" --timepos="1550:950";
