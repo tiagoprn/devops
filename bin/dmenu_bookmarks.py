a7c5f264446ee15be80aa765bd74e30d06939d63
tiago.paranhos <tiagoprn@gmail.com>
2019-03-27 18:27:35 -0300

    WIP: calling dmenu

--- bin/dmenu_bookmarks.py
+++ bin/dmenu_bookmarks.py
@@ -13,7 +13,20 @@ if __name__ == "__main__":
     bookmarks_list = get_bookmarks_list()
     lines = bookmarks_list.split('\n')
 
-    for line in lines:
-        print(line)
+    options = {}
 
-    # TODO: call fzf and dmenu like on the shellscript "search_posts_and_trigger_vim_by_file_contents.sh"
+    for line in lines:
+        splitted_line = line.split()
+        if not splitted_line:
+            continue
+        alias = splitted_line[0]
+        url = splitted_line[1]
+        options[alias] = url
+
+    dmenu_options = sorted(options.keys())
+
+    # TODO: the menu must be piped from printing dmenu_options above. E.g. how to run on the shell:
+    # $ printf 'eu\ntu\nele\n' | dmenu -b
+    dmenu_output = subprocess.run(['dmenu', '-b', ''.join(dmenu_options)], stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    # cat .BrowserBookmarks | awk '{print $1}' | dmenu -l 50 (the list lenght here)
