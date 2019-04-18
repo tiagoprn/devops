49637c3c16590cfc718b3bb2aa03416b9fba49fe
tiago.paranhos <tiagoprn@gmail.com>
2019-04-17 22:14:31 -0300

    Finished script.

--- bin/rofi/bookmarks.py
+++ bin/rofi/bookmarks.py
@@ -3,10 +3,8 @@
 # TODO: migrate to python-rofi: https://github.com/bcbnz/python-rofi
 import glob
 import os
-import subprocess
 import sys
 
-import dmenu
 from rofi import Rofi
 
 BROWSERS = {
@@ -21,34 +19,6 @@ def get_bookmark_files():
     return [filename for filename in \
             glob.iglob(f'{BOOKMARKS_ROOT}/**/.BrowserBookmarks',
                        recursive=True)]
-
-
-def main():
-    chosen_title = dmenu.show(urls.keys(),
-                              prompt='Choose a bookmark',
-                              lines=20)
-    if not chosen_title:
-        print('No title chosen, quitting now.')
-        sys.exit(0)
-
-    chosen_url = urls[chosen_title]
-    print(f'chosen_url={chosen_url}')
-
-    browser = dmenu.show(BROWSERS.keys(), prompt='Choose the browser:')
-
-    if not browser:
-        print('No browser chosen, quitting now.')
-        sys.exit(0)
-
-    command = f'notify-send --urgency low "Opening {browser} ' \
-              f'with bookmark {chosen_title}"...'
-    os.system(command)
-
-    if 'firefox' in browser.lower():
-        command = f"{BROWSERS[browser]} --new-tab {chosen_url} &"
-    else:
-        command = f"{BROWSERS[browser]} {chosen_url} &"
-    os.system(command)
 
 
 def get_bookmarks():
@@ -83,7 +53,34 @@ if __name__ == "__main__":
         print('cancelled')
         sys.exit(0)
 
-    print('Moving on...')
-    url = bookmarks_urls[bookmarks_list[selected]]
+    selected_bookmark = bookmarks_list[selected]
+    print(f'selected_bookmark={selected_bookmark}')
+    chosen_url = bookmarks_urls[selected_bookmark]
+    print(f'chosen_url={chosen_url}')
 
-    print(f'Opening url: {url}')
+    browsers = BROWSERS.keys()
+    browsers_keys = []
+    for index in BROWSERS:
+        browsers_keys.append(index)
+
+    selected, keyboard_key = rofi_client.select('Open on what browser?',
+                                                browsers)
+    print(f'keyboard_key pressed={keyboard_key}')
+
+    if keyboard_key == -1:
+        print('No browser chosen, quitting now.')
+        sys.exit(0)
+
+    print(f'selected={selected}')
+
+    selected_browser = BROWSERS[browsers_keys[selected]]
+    print(f'selected_browser={selected_browser}')
+    command = f'notify-send --urgency low "Opening {selected_browser} ' \
+              f'with url {selected_bookmark}"...'
+    os.system(command)
+
+    if 'firefox' in selected_browser.lower():
+        command = f"{selected_browser} --new-tab {chosen_url} &"
+    else:
+        command = f"{selected_browser} {chosen_url} &"
+    os.system(command)
