6bf7dfd14c235ee6c00fc3142fdaf212d04752d4
tiago.paranhos <tiagoprn@gmail.com>
2019-05-29 08:21:52 -0300

    Changed battery script to autodetect battery path.

--- bin/get_battery_info_new.sh
+++ bin/get_battery_info_new.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-PERCENTAGE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i 'percentage'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' )
-STATE=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i 'state'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' )
+BAT=$(upower -e | grep BAT)
+PERCENTAGE=$(upower -i $BAT | grep -i 'percentage'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' )
+STATE=$(upower -i $BAT | grep -i 'state'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//' )
 REMAINING=''
 if [ "$STATE" = "discharging" ]; then
-    REMAINING="(0% in $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i 'time to empty'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//'))"
+    REMAINING="(0% in $(upower -i $BAT | grep -i 'time to empty'| cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//'))"
 fi
 echo "$PERCENTAGE, $STATE $REMAINING"
