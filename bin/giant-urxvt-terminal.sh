a464f9aada309cb79e40e094ec3beb31bccbd01e
tiago.paranhos <tiagoprn@gmail.com>
2019-09-17 21:57:55 -0300

    Added new terminal command to i3.

---
+++ bin/giant-urxvt-terminal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+notify-send 'Opening bigger urxvt terminal...'
+urxvt -fn 'xft:Dejavu Sans Mono:size=20,autohint:true:antialias:true' -name presentation -w 30 -b 30 -cd /storage/docs/notes
