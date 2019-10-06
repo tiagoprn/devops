de8a536c9458a030e948bea930b1b268d8897617
tiago.paranhos <tiagoprn@gmail.com>
2019-10-06 19:40:57 -0300

    Changing font to iosevka.

--- bin/giant-urxvt-terminal.sh
+++ bin/giant-urxvt-terminal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 notify-send 'Opening bigger urxvt terminal...'
-urxvt -fn 'xft:Input Mono Condensed:size=16,autohint:true:antialias:true' -name presentation -w 30 -b 30 -cd /storage/docs/notes
+urxvt -fn 'xft:Iosevka:size=16,autohint:true:antialias:true' -name presentation -w 30 -b 30 -cd /storage/docs/notes
