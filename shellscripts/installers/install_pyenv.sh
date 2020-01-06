0ec7e588a7e8f35c4641314da10dbcd8bc35c0d2
tiago.paranhos <tiagoprn@gmail.com>
2020-01-06 08:23:06 -0300

    Adding functional pyenv installer.

---
+++ shellscripts/installers/install_pyenv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PYENV_ROOT="$HOME/.pyenv"
+PYENV_BIN=$PYENV_ROOT/bin/pyenv
+
+if [ -f $PYENV_BIN ];
+then
+    echo "UPGRADING pyenv..."
+    cd $PYENV_ROOT && git fetch
+    echo "Now, you must do a `git checkout` on the last tag."
+    # git checkout $(git tag)
+    echo "FINISHED."
+else
+    echo "INSTALLING pyenv..."
+    git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
+    echo "If you installed from other source than this repository, apply the changes from my files
+    .bash_environment (PYENV named environment variables) and .bashrc (pyenv initialization) to your
+    .bashrc."
+    echo "FINISHED. Quit this shell or start another one now."
+fi
