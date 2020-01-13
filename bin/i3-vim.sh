727cf1045f87b6999a676cf6b382ffb0d534d0ec
tiago.paranhos <tiagoprn@gmail.com>
2020-01-13 07:08:34 -0300

    Changing the i3 scratchpads htop and vim to use kitty as the terminal emulator.

--- bin/i3-vim.sh
+++ bin/i3-vim.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
+# urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
+kitty --name vim --hold bash -c 'vim /storage/docs/notes/personal/misc.md'
+
