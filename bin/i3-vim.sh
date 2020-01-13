f97669667db6e139fe50d8ac0680c7ced0affac9
tiago.paranhos <tiagoprn@gmail.com>
2020-01-13 07:16:21 -0300

    Updating vim scratchpad shellscript.

--- bin/i3-vim.sh
+++ bin/i3-vim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
-kitty --name vim --hold bash -c 'vim /storage/docs/notes/personal/misc.md'
+kitty --name vim --hold bash -c 'TERM=rxvt-unicode-256color vim /storage/docs/notes/personal/misc.md'
 
