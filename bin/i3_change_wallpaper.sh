4f0236fe68f01239b7c4d8f7839f2375bb4284b5
tiago.paranhos <tiagoprn@gmail.com>
2020-01-13 06:39:25 -0300

    Changed script to change wallpaper.

--- bin/i3_change_wallpaper.sh
+++ bin/i3_change_wallpaper.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# If there is not an image fallback.png on ~/Wallpapers, set the wallpaper 
+# If there wallpaper was not previously set by feh, set the wallpaper
 # to the most recent file at ~/Wallpapers (based on the file timestamp)
 
 WALLPAPERS_PATH=$HOME/Wallpapers
-FALLBACK_IMAGE=$WALLPAPERS_PATH/fallback.png
+FEH_BG=$HOME/.fehbg
 
-if [ ! -f $FALLBACK_IMAGE ]; then
+if [ -f $FEH_BG ]; then
+    /bin/bash $FEH_BG
+else
     echo "Setting the wallpaper to the most recent one at $WALLPAPERS_PATH..."
     feh --bg-scale $(find ~/Wallpapers -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)
-else
-    echo "Since the fallback image is at $FALLBACK_IMAGE , setting it as the wallpaper."
-    feh --bg-scale $HOME/Wallpapers/fallback.png        
 fi
 
