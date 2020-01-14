8828c46ed35c1f6636965bc90627bf48c15531b4
tiago.paranhos <tiagoprn@gmail.com>
2020-01-14 08:24:21 -0300

    Better feedback message.

--- shellscripts/multipass/create-and-start-multipass-vm.sh
+++ shellscripts/multipass/create-and-start-multipass-vm.sh
@@ -3,5 +3,5 @@ IMAGE_NAME=ubuntu-lts-$(date +%Y%m%d-%H%M%S)
 ./generate-cloud-config.sh
 multipass launch -n $IMAGE_NAME --cloud-init cloud-config.yaml && \
 multipass list && \
-echo "Image created and started as $IMAGE_NAME".
+echo -e "\n---\nFINISHED.I mage created and started as $IMAGE_NAME".
 
