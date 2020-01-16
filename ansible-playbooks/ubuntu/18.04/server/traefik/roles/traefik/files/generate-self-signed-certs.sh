637c4f10987ed1e3007e81bfe71076e6ad7125d5
tiago.paranhos <tiagoprn@gmail.com>
2020-01-16 18:27:02 -0300

    Fixing syntax bug.

--- ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
+++ ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
@@ -3,12 +3,9 @@ CERTS_DIR=/opt/containers/traefik/certs
 CERTIFICATE_FILE=$CERTS_DIR/tls.crt
 KEY_FILE==$CERTS_DIR/tls.key
 
-mkdir -p $CERTS_DIR
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERTIFICATE_FILE -subj "/C=BR/ST=Sao_Paulo/L=Sao_Paulo/O=My·Domination·Company/OU=IT·Department/CN=localhost"
-
-echo "Certificates created at $CERTS_DIR."
-
-ls -l $CERTS_DIR/tls*
-
+mkdir -p $CERTS_DIR &&\
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERTIFICATE_FILE -subj "/C=BR/ST=Sao_Paulo/L=Sao_Paulo/O=My·Domination·Company/OU=IT·Department/CN=localhost" &&\
+echo "Certificates created at $CERTS_DIR." &&\
+ls -l $CERTS_DIR/tls* &&\
 echo "FINISHED."
 
