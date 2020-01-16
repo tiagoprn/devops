b4a26c45b18c6505379b464c322c8659d3046c1f
tiago.paranhos <tiagoprn@gmail.com>
2020-01-16 18:23:27 -0300

    Fixed bug on the script.

--- ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
+++ ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
@@ -8,7 +8,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERT
 
 echo "Certificates created at $CERTS_DIR."
 
-ls -l tls.*
+ls -l $CERTS_DIR/tls*
 
 echo "FINISHED."
 
