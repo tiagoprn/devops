fdc9524da276ce13796405c7a04756cab9c55649
tiago.paranhos <tiagoprn@gmail.com>
2020-01-17 07:26:41 -0300

    First working version of the traefik script with sample apps.

--- ansible-playbooks/ubuntu/18.04/server/traefik/roles/TODO.md
+++
@@ -1,2 +0,0 @@
-Like it was done on the docker playbook with portainer, generate the password
-for the admin user on `traefik.toml`.
--- ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
+++ ansible-playbooks/ubuntu/18.04/server/traefik/roles/traefik/files/generate-self-signed-certs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 CERTS_DIR=/opt/containers/traefik/certs
 CERTIFICATE_FILE=$CERTS_DIR/tls.crt
-KEY_FILE==$CERTS_DIR/tls.key
+KEY_FILE=$CERTS_DIR/tls.key
 
+echo "Creating certificates directory $CERTS_DIR..." && \
 mkdir -p $CERTS_DIR &&\
+echo "Issuing certificate..."&& \
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $KEY_FILE -out $CERTIFICATE_FILE -subj "/C=BR/ST=Sao_Paulo/L=Sao_Paulo/O=My·Domination·Company/OU=IT·Department/CN=localhost" &&\
-echo "Certificates created at $CERTS_DIR." &&\
+echo "Certificate created at $CERTS_DIR." &&\
 ls -l $CERTS_DIR/tls* &&\
 echo "FINISHED."
 
