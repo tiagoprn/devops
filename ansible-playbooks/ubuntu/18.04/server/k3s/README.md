3269effeed8c57c967c3cde1b8f001da4d8d24f0
tiago.paranhos <tiagoprn@gmail.com>
2020-01-18 15:12:13 -0300

    Updating k3s README and updating multipass to have more control of the generated VM.

--- ansible-playbooks/ubuntu/18.04/server/k3s/README.md
+++ ansible-playbooks/ubuntu/18.04/server/k3s/README.md
@@ -9,3 +9,5 @@ provisionate the server.
 # REFERENCES
 - https://medium.com/@marcovillarreal_40011/cheap-and-local-kubernetes-playground-with-k3s-helm-5a0e2a110de9
 - https://www.freshbrewed.science/ubuntu-multipass-better-than-docker/index.html
+- https://github.com/rancher/k3s/issues/977
+- https://help.replicated.com/community/t/managing-firewalls-with-ufw-on-kubernetes/230
--- shellscripts/multipass/create-and-start-multipass-vm.sh
+++ shellscripts/multipass/create-and-start-multipass-vm.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
-IMAGE_NAME=ubuntu-lts-$(date +%Y%m%d-%H%M%S)
+
+SYNTAX="./create-and-start-multipass-vm.sh -c 1 -d 5G -m 1G"
+
+while getopts c:d:m: option; do
+	case $option in
+		c) CPUS=${OPTARG};;
+		d) DISK=${OPTARG};;
+		m) MEMORY=${OPTARG};;
+	esac
+done
+
+echo 'Parsing script parameters...'
+
+if [[ -z $CPUS ]]; then
+    echo -e "Missing -c parameter. Assuming default value.";
+    CPUS=1
+fi
+
+if [[ -z $DISK ]]; then
+    echo -e "Missing -d parameter. Assuming default value.";
+    DISK=5G
+fi
+
+if [[ -z $MEMORY ]]; then
+    echo -e "Missing -m parameter. Assuming default value.";
+    MEMORY=1G
+fi
+
+VM_NAME=ubuntu-lts-$(date +%Y%m%d-%H%M%S)
 ./generate-cloud-config.sh
-multipass launch -n $IMAGE_NAME --cloud-init cloud-config.yaml && \
+multipass launch -n $VM_NAME -c $CPUS -d $DISK -m $MEMORY --cloud-init cloud-config.yaml && \
 multipass list && \
-echo -e "\n---\nFINISHED. Image created and started as $IMAGE_NAME".
+
+echo -e "\n---\nFINISHED. VM $VM_NAME created with CPUS=$CPUS, DISK=$DISK and MEMORY=$MEMORY. Have fun! o/".
 
