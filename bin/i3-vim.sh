f1bcc25e55b516d1cfd0aaefb652035bef3cbb16
tiago.paranhos <tiagoprn@gmail.com>
2020-01-21 08:38:58 -0300

    Changed terminal variable to support insert/visual modes on bash and vim.

--- bin/i3-vim.sh
+++ bin/i3-vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
 TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
-kitty --name vim --hold bash -c "TERM=rxvt-unicode-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml"
+kitty --name vim --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml"
 
