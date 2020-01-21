603148b623ece66e3d4129b15fe1185e906e1283
tiago.paranhos <tiagoprn@gmail.com>
2020-01-21 08:35:52 -0300

    Changed i3-vim to support a terminal on below to interact with the repo on a tmux window.

--- bin/i3-vim.sh
+++ bin/i3-vim.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
-kitty --name vim --hold bash -c 'TERM=rxvt-unicode-256color vim /storage/docs/notes/personal/misc.md'
+TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
+kitty --name vim --hold bash -c "TERM=rxvt-unicode-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml"
 
