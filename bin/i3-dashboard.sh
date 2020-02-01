08a1384ffb4aed00bc64e81237e8515470a1f560
tiago.paranhos <tiagoprn@gmail.com>
2020-02-01 14:22:16 -0300

    Finished tmux session configuration.

--- bin/i3-dashboard.sh
+++ bin/i3-dashboard.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
-kitty --name vim --hold bash -c "TERM=screen-256color $TMUXP_BIN load
-/storage/src/devops/tmuxp/dashboard.yml"
+kitty --name cli_dashboard --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/dashboard.yml"
 
