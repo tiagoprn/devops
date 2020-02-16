dcf6bc61a5f333c4ea03c680a007e0c094ca0a71
tiago.paranhos <tiagoprn@gmail.com>
2020-02-16 12:00:43 -0300

    Change script to resize to manually do that through gparted.

--- raspberry/dockerpi/resize-raspbian-img.sh
+++ raspberry/dockerpi/resize-raspbian-img.sh
@@ -7,14 +7,18 @@ echo 'Setup the image as a loopback device, so it can be manipulated with tools 
 LOOPBACK_MOUNT=$(sudo udisksctl loop-setup -f $FULL_RASPBIAN_IMAGE_PATH | grep -o -m 1 -P -e "/dev/loop[0-9]+")
 echo "Image setup as loopback mount at $LOOPBACK_MOUNT"
 
+# exit 1  # REMOVE on the final version, just testing here...
+
 echo 'Listing block devices...'
 lsblk
 
-echo 'Preparing to resize the 2nd partition on the image loopback device...'
-sudo e2fsck -y -f "$LOOPBACK_MOUNT"p2
+# echo 'Preparing to resize the 2nd partition on the image loopback device...'
+# sudo e2fsck -y -f "$LOOPBACK_MOUNT"p2
+# echo 'Resize the 2nd partition, the one with the filesystem...'
+# sudo parted $LOOPBACK_MOUNT resizepart 2
 
-echo 'Resize the 2nd partition, the one with the filesystem. (learned that through gparted)'
-sudo resize2fs -p "$LOOPBACK_MOUNT"p2
+echo 'Will run gparted now on the loopback device, you must manually resize the partition to the grown space...'
+sudo gparted $LOOPBACK_MOUNT
 
 echo 'Delete the mapped loopback device'
 sudo udisksctl loop-delete -b $LOOPBACK_MOUNT
