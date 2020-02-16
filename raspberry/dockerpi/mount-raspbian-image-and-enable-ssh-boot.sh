dc22224cbb7b966b712606f37ef1c38accf26e27
tiago.paranhos <tiagoprn@gmail.com>
2020-02-16 09:05:44 -0300

    Finished script to modify default raspbian image.

--- raspberry/dockerpi/mount-raspbian-image-and-enable-ssh-boot.sh
+++ raspberry/dockerpi/mount-raspbian-image-and-enable-ssh-boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # modify the raspbian image to allow auto start of the ssh daemon
-FULL_RASPBIAN_IMAGE_PATH=2020-02-05-raspbian-buster-lite.img
+echo "Mount raspbian image at $FULL_RASPBIAN_IMAGE_PATH..."
 bootLoopDevice="$(sudo kpartx -avs $FULL_RASPBIAN_IMAGE_PATH | head -n 1 | awk '{print $3}')"
 # add ssh file to boot partition to activate ssh directly during raspbian start
 mkdir boot
@@ -10,3 +10,4 @@ sudo umount boot
 sudo rm -fr boot
 # FIXME: change raspi hostname to rpi-gateway
 sudo kpartx -d $FULL_RASPBIAN_IMAGE_PATH
+echo "Finished mounting and modifying raspbian image at $FULL_RASPBIAN_IMAGE_PATH."
