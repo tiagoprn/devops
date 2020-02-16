17dc98b58ea239239008c6adcf07f1452a955754
tiago.paranhos <tiagoprn@gmail.com>
2020-02-16 17:56:38 -0300

    Adding small enhancements.

--- raspberry/dockerpi/first_boot.sh
+++ raspberry/dockerpi/first_boot.sh
@@ -3,9 +3,10 @@
 echo 'Running first login script...[WAIT]'
 
 echo 'Configuring locale...[WAIT]'
-sudo bash -c "cd /etc/default && mv locale locale.ORIG && wget https://raw.githubusercontent.com/tiagoprn/devops/master/raspberry/dockerpi/config/locale "
-sudo bash -c "cd /etc && mv locale.gen locale.gen.ORIG && wget https://raw.githubusercontent.com/tiagoprn/devops/master/raspberry/dockerpi/config/locale.gen"
-sudo bash -c "source /etc/default/locale && sudo locale-gen en_US.UTF-8 && sudo update-locale en_US.UTF-8"
+sudo su -c "cd /etc/default && mv locale locale.ORIG && wget https://raw.githubusercontent.com/tiagoprn/devops/master/raspberry/dockerpi/config/locale "
+sudo su -c "cd /etc && mv locale.gen locale.gen.ORIG && wget https://raw.githubusercontent.com/tiagoprn/devops/master/raspberry/dockerpi/config/locale.gen"
+sudo su -c "source /etc/default/locale && sudo locale-gen en_US.UTF-8 && sudo update-locale en_US.UTF-8"
+sudo su -c "Current locale is: $(echo $LC_ALL)."
 echo 'Configuring locale...[DONE]'
 
 # TODO: authorize ssh key for the pi user
