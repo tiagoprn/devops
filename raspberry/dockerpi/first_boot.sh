26a160cfc8bad561b63efa913af3da8ac3d39b95
tiago.paranhos <tiagoprn@gmail.com>
2020-02-17 18:34:41 -0300

    Added note to future self.

--- raspberry/dockerpi/first_boot.sh
+++ raspberry/dockerpi/first_boot.sh
@@ -21,6 +21,9 @@ sudo su -c "chmod 644 /home/pi/.ssh/authorized_keys"
 sudo su -c "chown 1000:1000 /home/pi/.ssh/authorized_keys"
 echo "Authorize ssh key for the pi user...[DONE]"
 
+# This is a sinalizer that this script has already run.
+# May be used every time I need to check the first boot
+# setup process has already finished.
 touch /run/first_boot_passed
 
 echo 'Running first login script...[DONE]'
