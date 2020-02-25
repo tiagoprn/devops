464588ed81387defd73a2f11e9862ed20d941629
tiago.paranhos <tiagoprn@gmail.com>
2020-02-25 13:06:04 -0300

    Finished command output parsing.

--- bin/keyboard-leds.sh
+++ bin/keyboard-leds.sh
@@ -4,4 +4,4 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]')
-echo -e "$STATUS" | GREP_COLORS='ms=01;32' grep -E --color 'ON|$'
+echo -e "$STATUS" | sed 's/,/ | /g' | GREP_COLORS='ms=01;32' grep -E --color 'ON|$'
