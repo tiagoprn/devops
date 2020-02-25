0109c404e935833085004290cc4897a3ee5f6e26
tiago.paranhos <tiagoprn@gmail.com>
2020-02-25 14:02:00 -0300

    Finished the new keyboard indicator.

--- bin/keyboard-leds.sh
+++ bin/keyboard-leds.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
 
-STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]')
-echo -e "$STATUS" | sed 's/,/ | /g' | GREP_COLORS='ms=01;32' grep -E --color 'ON|$'
+STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]' | sed 's/,/ | /g')
+
+if [[ "$STATUS" == *"ON"* ]]; then
+    COLOR='#00FF00';
+else
+    COLOR='#FFFFFF';
+fi
+
+# To change color of the text on the block, the output must have three lines and the third one must
+# be a hexadecimal color.
+echo -e "$STATUS\n\n$COLOR";
