45f21193e70e1754be37b6b061a7a4bcf3f0ff86
tiago.paranhos <tiagoprn@gmail.com>
2020-03-04 08:23:18 -0300

    Adding new tmux session.

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -6,3 +6,4 @@ tmuxp load -d /storage/src/devops/tmuxp/home.yml
 tmuxp load -d /storage/src/devops/tmuxp/dev.yml
 tmuxp load -d /storage/src/devops/tmuxp/core-utils.yml
 tmuxp load -d /storage/src/devops/tmuxp/db-utils.yml
+tmuxp load -d /storage/src/devops/tmuxp/pyenv-python-project.yml
