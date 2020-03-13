7bdb66f0236d94ddb9d2538f2d7247587c3e9c93
tiago.paranhos <tiagoprn@gmail.com>
2020-03-13 18:15:29 -0300

    Fixing prometheus setup.

--- ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
+++ ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 firewall-cmd --permanent --zone=public --add-port=19999/tcp
+firewall-cmd --permanent --zone=public --add-port=9090/tcp
+firewall-cmd --permanent --zone=public --add-port=9100/tcp
+
 # ufw allow 19999/tcp
 
 echo "FINISHED!"
