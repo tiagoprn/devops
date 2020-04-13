f858bc21afae5bebec8a6229e784bc2f042150f5
tiago.paranhos <tiagoprn@gmail.com>
2020-04-13 19:07:52 -0300

    monitoring playbook: TODO item 1 finished.

--- ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
+++ ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# ports only allowed on localhost
+firewall-cmd --permanent --zone=internal --add-interface=lo
+firewall-cmd --permanent --zone=internal --add-port=9100/tcp
+
+# ports available on LAN/WAN
 firewall-cmd --permanent --zone=public --add-port=19999/tcp
 firewall-cmd --permanent --zone=public --add-port=9090/tcp
-firewall-cmd --permanent --zone=public --add-port=9100/tcp
 firewall-cmd --permanent --zone=public --add-port=9093/tcp
 
 # ufw allow 19999/tcp
