db4cf7e253173b2f5ffe11c186183758140c4309
tiago.paranhos <tiagoprn@gmail.com>
2020-04-20 10:40:55 -0300

    Reconfiguring firewall to only allow the ports to be exposed locally.

--- ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
+++ ansible-playbooks/ubuntu/18.04/server/monitoring/roles/firewalld/files/firewall-rules.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# ports only allowed on localhost
+# Adding an internal zone to the firewall, mapped to localhost interface
 firewall-cmd --permanent --zone=internal --add-interface=lo
+
+
+# Ports only allowed on localhost
+## node_exporter:
 firewall-cmd --permanent --zone=internal --add-port=9100/tcp
+## netdata:
+firewall-cmd --permanent --zone=internal --add-port=19999/tcp
+## prometheus:
+firewall-cmd --permanent --zone=internal --add-port=9090/tcp
+## alertmanager:
+firewall-cmd --permanent --zone=internal --add-port=9093/tcp
 
-# ports available on LAN/WAN
-firewall-cmd --permanent --zone=public --add-port=19999/tcp
-firewall-cmd --permanent --zone=public --add-port=9090/tcp
-firewall-cmd --permanent --zone=public --add-port=9093/tcp
 
+# Ports available on LAN/WAN
+## netdata:
+# firewall-cmd --permanent --zone=public --add-port=19999/tcp
+## prometheus:
+# firewall-cmd --permanent --zone=public --add-port=9090/tcp
+## alertmanager:
+# firewall-cmd --permanent --zone=public --add-port=9093/tcp
+
+
+# Example to set ubuntu's ufw:
 # ufw allow 19999/tcp
 
 echo "FINISHED!"
