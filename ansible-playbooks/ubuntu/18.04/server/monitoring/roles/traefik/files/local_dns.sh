e083b5e24893b933d5d424059679797e9fb9b922
tiago.paranhos <tiagoprn@gmail.com>
2020-04-21 10:38:07 -0300

    Adding prometheus and alertmanager to traefik.

--- ansible-playbooks/ubuntu/18.04/server/monitoring/roles/traefik/files/local_dns.sh
+++ ansible-playbooks/ubuntu/18.04/server/monitoring/roles/traefik/files/local_dns.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "# The following lines are desired so that you can host apps on your machine (e.g., traefik)" >> /etc/hosts
-echo "127.0.0.1 www.example.com example.com traefik.example.com netdata.example.com" >> /etc/hosts
-echo "1.1.1.1 www.example.com example.com traefik.example.com netdata.example.com"  >> /etc/hosts
+echo "127.0.0.1 www.example.com example.com traefik.example.com netdata.example.com prometheus.example.com alertmanager.example.com" >> /etc/hosts
+echo "1.1.1.1 www.example.com example.com traefik.example.com netdata.example.com prometheus.example.com alertmanager.example.com"  >> /etc/hosts
 
