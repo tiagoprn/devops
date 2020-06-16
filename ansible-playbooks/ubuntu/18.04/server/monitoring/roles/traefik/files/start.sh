0b27f3d547bd78bccde46d6062c4945ad77887e0
tiago.paranhos <tiagoprn@gmail.com>
2020-06-16 07:53:19 -0300

    Finished random password generation for traefik dashboard.

--- ansible-playbooks/ubuntu/18.04/server/monitoring/roles/traefik/files/start.sh
+++ ansible-playbooks/ubuntu/18.04/server/monitoring/roles/traefik/files/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TRAEFIK=$(docker ps | grep traefik | awk '{print $1}')
+PASSWORD=$(cat /opt/containers/traefik/admin.passwd)
 if [ $TRAEFIK  ]
 then
     printf 'traefik is already running, nothing to be done here.'
@@ -8,6 +9,7 @@ else
     printf 'Starting traefik in the background...\n'
     cd /opt/containers/traefik && \
         docker-compose up -d && \
-        echo -e 'Traefik started. It can be reached through: https://traefik.example.com, with user/password=admin/12345678. '
+        echo -e "Traefik started. It can be reached through:
+            https://traefik.example.com, with user/password=traefik/$PASSWORD. "
 fi
 
