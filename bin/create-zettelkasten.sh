8cffb2b492dee92f5664d4bc6e90511c21406319
tiago.paranhos <tiagoprn@gmail.com>
2020-06-24 08:55:43 -0300

    Added command to create a zettelkasten

---
+++ bin/create-zettelkasten.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Creates a zettelkasten file inside the current folder
+# This must work seamlessly from i3, I will add a mapping to it.
+
+CARDS_DIR=/storage/docs/notes/zettelkasten/cards
+TIMESTAMP="$(date "+%Y-%m-%d-%H%M%S-%3N")"
+FILENAME=$CARDS_DIR/$TIMESTAMP.md
+
+vim "$FILENAME" +'normal!ggIztk'
+
