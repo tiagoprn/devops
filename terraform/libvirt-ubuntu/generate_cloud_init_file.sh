2d203680d97ac81708616119a7d9d0c59a625f87
tiago.paranhos <tiagoprn@gmail.com>
2020-07-01 09:25:04 -0300

    Changing cloud init file generation script.

--- terraform/libvirt-ubuntu/generate_cloud_init_file.sh
+++ terraform/libvirt-ubuntu/generate_cloud_init_file.sh
@@ -36,19 +36,12 @@ fqdn: $1.vbox.local
 
 # Users
 users:
-    - name: centos
+    - name: ubuntu
       groups: ['${SUDOGROUP}']
       shell: /bin/bash
       sudo: ALL=(ALL) NOPASSWD:ALL
       ssh_authorized_keys:
         - ${KEY}
-    - name: ops
-      groups: ['${SUDOGROUP}']
-      shell: /bin/bash
-      sudo: ALL=(ALL) NOPASSWD:ALL
-      ssh_authorized_keys:
-        - ${KEY}
-
 
 bootcmd:
   - echo 'My ip addresses are '$(hostname --ip-address)
