3b535dc52f8462247376599c432cdf0052936b4d
tiago.paranhos <tiagoprn@gmail.com>
2020-07-01 08:40:42 -0300

    Disabling network config.

--- terraform/libvirt-ubuntu/ubuntu.tf
+++ terraform/libvirt-ubuntu/ubuntu.tf
@@ -21,9 +21,9 @@ data "template_file" "user_data" {
   template = file("${path.module}/cloud_init.cfg")
 }
 
-data "template_file" "network_config" {
-  template = file("${path.module}/network_config.cfg")
-}
+# data "template_file" "network_config" {
+#   template = file("${path.module}/network_config.cfg")
+# }
 
 # for more info about paramater check this out
 # https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
@@ -32,7 +32,7 @@ data "template_file" "network_config" {
 resource "libvirt_cloudinit_disk" "commoninit" {
   name           = "commoninit.iso"
   user_data      = data.template_file.user_data.rendered
-  network_config = data.template_file.network_config.rendered
+  # network_config = data.template_file.network_config.rendered
   pool           = libvirt_pool.ubuntu.name
 }
 
