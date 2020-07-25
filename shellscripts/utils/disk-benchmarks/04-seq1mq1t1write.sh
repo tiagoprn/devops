8c00cae3350920e0ae433a0343d914cc4ff05393
tiago.paranhos <tiagoprn@gmail.com>
2020-07-25 13:08:49 -0300

    Adding scripts

---
+++ shellscripts/utils/disk-benchmarks/04-seq1mq1t1write.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TARGET="$1"
+me=`basename "$0"`
+#--output-format=json
+fio --loops=1 --size=1024m --filename="$TARGET/fiomark.tmp" --stonewall --ioengine=libaio --direct=1 --zero_buffers=0 \
+--name="$me" --bs=1m --iodepth=1 --numjobs=1 --rw=write --output-format=json
+
+rm "$TARGET/fiomark.tmp"
---
+++ shellscripts/utils/disk-benchmarks/README.md
@@ -0,0 +1,2 @@
+Based on the original work from here: <https://github.com/fredcox/crazydiskmark>
+
