57150c065be8492533aba9294fcb6cfe225f2a8a
tiago.paranhos <tiagoprn@gmail.com>
2020-09-10 20:09:22 -0300

    Updated the shellscripts.

--- bin/download_random_commitstrip.sh
+++ bin/download_random_commitstrip.sh
@@ -14,6 +14,8 @@ mkdir -p $DOWNLOAD_ROOT
 notify-send --urgency critical "Getting comic URL..."
 COMIC=$(wget -qO /dev/stdout http://www.commitstrip.com/?random=1 | grep uploads | grep src | grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|].jpg' | uniq)
 
+echo "COMIC: $COMIC"
+
 DOWNLOAD_TO=$DOWNLOAD_ROOT/$TIMESTAMP.jpg
 
 notify-send --urgency critical "Downloading comic to $DOWNLOAD_TO ..."
