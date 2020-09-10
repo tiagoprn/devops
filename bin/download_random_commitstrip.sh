4c0d3216b4a195a692635565397e721d10d411e4
tiago.paranhos <tiagoprn@gmail.com>
2020-09-10 20:58:02 -0300

    Updating script that downloads from commitstrip.

--- bin/download_random_commitstrip.sh
+++ bin/download_random_commitstrip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TIMESTAMP="$(date "+%Y%m%d.%H%M.%S")"
-DOWNLOAD_ROOT=$HOME/tmp
+DOWNLOAD_ROOT=$HOME/tmp/commitstrip
 
 CURRENT_SCREEN_RESOLUTION=$(xrandr | grep \* | cut -d' ' -f4 -)
 echo "resolution=$CURRENT_SCREEN_RESOLUTION"
@@ -16,25 +16,33 @@ COMIC=$(wget -qO /dev/stdout http://www.commitstrip.com/?random=1 | grep uploads
 
 echo "COMIC: $COMIC"
 
-DOWNLOAD_TO=$DOWNLOAD_ROOT/$TIMESTAMP.jpg
+FILENAME=$(echo $COMIC | cut -d '/' -f 8)
+
+echo "FILENAME: $FILENAME"
+
+DOWNLOAD_TO=$DOWNLOAD_ROOT/$FILENAME
+
+# exit 1  # TODO: remove  after finishing up
 
 notify-send --urgency critical "Downloading comic to $DOWNLOAD_TO ..."
 curl "$COMIC" -o $DOWNLOAD_TO
 
 IMAGE_WIDTH=$(identify -ping -format "%w" $DOWNLOAD_TO)
 IMAGE_HEIGHT=$(identify -ping -format "%h" $DOWNLOAD_TO)
-notify-send --urgency critical "$DOWNLOAD_TO width=$IMAGE_WIDTH px, height=$IMAGE_HEIGHT px"
+echo "$DOWNLOAD_TO width=$IMAGE_WIDTH px, height=$IMAGE_HEIGHT px"
 
 MAXIMUM_HEIGHT=$SCREEN_HEIGHT
 X_MAXIMUM_HEIGHT=x$MAXIMUM_HEIGHT
 
+echo "IMAGE_HEIGHT=$IMAGE_HEIGHT"
 echo "X_MAXIMUM_HEIGHT=$X_MAXIMUM_HEIGHT"
 
 HEIGHT_SURPASSED=$(python3 -c "print('1' if $IMAGE_HEIGHT > $MAXIMUM_HEIGHT else '0')")
-notify-send --urgency critical "HEIGHT_SURPASSED_MAXIMUM=$HEIGHT_SURPASSED"
+
+echo "HEIGHT_SURPASSED_MAXIMUM=$HEIGHT_SURPASSED"
 
 if [ $HEIGHT_SURPASSED -eq '1' ]; then
-    notify-send --urgency critical "Resizing $DOWNLOAD_TO to $MAXIMUM_HEIGHT px height..."
+    echo "Resizing $DOWNLOAD_TO to $MAXIMUM_HEIGHT px height..."
     convert -resize $X_MAXIMUM_HEIGHT $DOWNLOAD_TO $DOWNLOAD_TO
 fi
 
