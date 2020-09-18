5fc61983548ea747c20a573c5a7a97e7071d0dfa
tiago.paranhos <tiagoprn@gmail.com>
2020-09-18 06:03:20 -0300

    fix: removing what was not a command.

--- shellscripts/installers/install_cockpit_on_raspbian.sh
+++ shellscripts/installers/install_cockpit_on_raspbian.sh
@@ -6,8 +6,8 @@ set -e
 # https://eercan.com/post/monitoring-configuration-of-your-linux-devices-machines-using-cockpit/
 
 apt-get install dirmngr --install-recommends
-run apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010
-run apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010
+apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553
 
 echo 'deb http://deb.debian.org/debian stretch-backports main' > \
      /etc/apt/sources.list.d/backports.list
