ad53db20ee66e50d001f1940dcaa7f71ad2cdb05
tiago.paranhos <tiagoprn@gmail.com>
2020-09-18 06:33:12 -0300

    feat: added more cockpit packages

--- shellscripts/installers/install_cockpit_on_raspbian.sh
+++ shellscripts/installers/install_cockpit_on_raspbian.sh
@@ -6,12 +6,12 @@ set -e
 # https://eercan.com/post/monitoring-configuration-of-your-linux-devices-machines-using-cockpit/
 
 apt-get install dirmngr --install-recommends
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010
-apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553
+apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010 || true
+apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553 || true
 
 echo 'deb http://deb.debian.org/debian stretch-backports main' > \
      /etc/apt/sources.list.d/backports.list
 apt-get update
-apt-get install cockpit -y
+apt-get install cockpit cockpit-bridge cockpit-dashboard cockpit-doc cockpit-docker cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-pcp cockpit-storaged cockpit-system cockpit-tests cockpit-ws -y
 
 echo 'Install finished. Cockpit can be accessed through port :9090.'
