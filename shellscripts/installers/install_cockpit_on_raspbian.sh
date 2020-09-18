cae7fe27e8bd59f756c0dcf223b91e3934ac2172
tiago.paranhos <tiagoprn@gmail.com>
2020-09-18 05:52:19 -0300

    feat: shellscripts/installers: added an installer for cockpit on raspbian.

---
+++ shellscripts/installers/install_cockpit_on_raspbian.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+# reference:
+# https://eercan.com/post/monitoring-configuration-of-your-linux-devices-machines-using-cockpit/
+
+apt-get install dirmngr --install-recommends
+run apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010
+run apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553
+
+echo 'deb http://deb.debian.org/debian stretch-backports main' > \
+     /etc/apt/sources.list.d/backports.list
+apt-get update
+apt-get install cockpit -y
+
+echo 'Install finished. Cockpit can be accessed through port :9090.'
