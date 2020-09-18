265dcc191eceb2c7ab96063a59a45ea08faf3139
tiago.paranhos <tiagoprn@gmail.com>
2020-09-18 07:47:43 -0300

    feat: cheats: created new vim cheat, shellscripts/installers: updated cockpit install.

--- shellscripts/installers/install_cockpit_on_raspbian.sh
+++ shellscripts/installers/install_cockpit_on_raspbian.sh
@@ -5,13 +5,17 @@ set -e
 # reference:
 # https://eercan.com/post/monitoring-configuration-of-your-linux-devices-machines-using-cockpit/
 
-apt-get install dirmngr --install-recommends
-apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010 || true
-apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553 || true
+# apt-get install dirmngr --install-recommends
+# apt-key adv --keyserver keyserver.ubuntu.com --recv 7638D0442B90D010 || true
+# apt-key adv --keyserver keyserver.ubuntu.com --recv 8B48AD6246925553 || true
 
-echo 'deb http://deb.debian.org/debian stretch-backports main' > \
-     /etc/apt/sources.list.d/backports.list
+# echo 'deb http://deb.debian.org/debian stretch-backports main' > \
+#      /etc/apt/sources.list.d/backports.list
 apt-get update
-apt-get install cockpit cockpit-bridge cockpit-dashboard cockpit-doc cockpit-docker cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-pcp cockpit-storaged cockpit-system cockpit-tests cockpit-ws -y
+
+apt-get install -y cockpit cockpit-bridge cockpit-dashboard \
+    cockpit-doc cockpit-docker cockpit-machines \
+    cockpit-networkmanager cockpit-packagekit cockpit-pcp \
+    cockpit-storaged cockpit-system cockpit-tests cockpit-ws
 
 echo 'Install finished. Cockpit can be accessed through port :9090.'
