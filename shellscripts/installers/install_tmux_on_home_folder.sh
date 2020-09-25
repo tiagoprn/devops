490f141f068a17aaf00593ff47f9ebd95330a571
tiago.paranhos <tiagoprn@gmail.com>
2020-09-25 08:00:42 -0300

    feat: tmux installer: upgraded to tmux 3.2rc2 (because of floating windows)

--- shellscripts/installers/install_tmux_on_home_folder.sh
+++ shellscripts/installers/install_tmux_on_home_folder.sh
@@ -12,7 +12,11 @@ set -e
 # install pre-requisite package
 sudo apt install -y libssl-dev
 
-TMUX_VERSION=3.1b
+TMUX_VERSION=3.2
+# set below to a blank string if the version is not RC
+IS_RC='-rc'
+RC_VERSION='-rc2'
+
 LIBEVENT_VERSION=2.1.12
 NCURSES_VERSION=6.1
 
@@ -22,7 +26,7 @@ cd $HOME/tmux_tmp
 
 # download source files for tmux, libevent, and ncurses
 # wget -O tmux-${TMUX_VERSION}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz/download
-wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz
+wget -O tmux-${TMUX_VERSION}.tar.gz https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}${IS_RC}/tmux-${TMUX_VERSION}${RC_VERSION}.tar.gz
 
 # wget https://github.com/downloads/libevent/libevent/libevent-${LIBEVENT_VERSION}-stable.tar.gz
 wget https://github.com/libevent/libevent/releases/download/release-${LIBEVENT_VERSION}-stable/libevent-${LIBEVENT_VERSION}-stable.tar.gz
@@ -55,7 +59,7 @@ cd ..
 # tmux     #
 ############
 tar xvzf tmux-${TMUX_VERSION}.tar.gz
-cd tmux-${TMUX_VERSION}
+cd tmux-${TMUX_VERSION}${RC_VERSION}
 ./configure CFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-L$HOME/local/lib -L$HOME/local/include/ncurses -L$HOME/local/include"
 CPPFLAGS="-I$HOME/local/include -I$HOME/local/include/ncurses" LDFLAGS="-static -L$HOME/local/include -L$HOME/local/include/ncurses -L$HOME/local/lib" make
 cp tmux $HOME/local/bin
