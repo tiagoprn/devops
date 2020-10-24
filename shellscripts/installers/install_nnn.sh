cb434ed1c6c456bcbe0e7e8a4cc7a4ee0d5e0c4d
tiago.paranhos <tiagoprn@gmail.com>
2020-10-24 09:46:32 -0300

    feat(shellscripts): (wip) nnn installer

--- shellscripts/installers/install_nnn.sh
+++ shellscripts/installers/install_nnn.sh
@@ -10,15 +10,13 @@
 set -e
 
 INSTALLER_PATH=/opt/installers/nnn
-sudo mkdir -p $INSTALLER_PATH
+mkdir -p $INSTALLER_PATH
 
 VERSION=3.4
 
-# download source files for tmux, libevent, and ncurses
-# wget -O tmux-${TMUX_VERSION}.tar.gz http://sourceforge.net/projects/tmux/files/tmux/tmux-${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz/download
-sudo wget -O $INSTALLER_PATH/nnn-${VERSION}.tar.gz https://github.com/jarun/nnn/archive/v${VERSION}.tar.gz
+# wget -O $INSTALLER_PATH/nnn-${VERSION}.tar.gz https://github.com/jarun/nnn/archive/v${VERSION}.tar.gz
 
-# cleanup
-rm -rf $HOME/tmux_tmp
+cd /opt/installers/nnn && tar xfzv nnn-${VERSION}.tar.gz
+
 
 echo "nnn sucessfully installed at ."
