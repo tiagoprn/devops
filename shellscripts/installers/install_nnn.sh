48d7133b2e64be390f62034c8b40fb73b61909e6
tiago.paranhos <tiagoprn@gmail.com>
2020-10-24 10:02:29 -0300

    feat(shellscripts: Finished nnn installer.

--- shellscripts/installers/install_nnn.sh
+++ shellscripts/installers/install_nnn.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# TODO: upgrade to tmux 3.x, test on kvm with ubuntu 18.
-
-# Script for installing tmux on systems where you don't have root access.
-# tmux will be installed in $HOME/local/bin.
-# It's assumed that wget and a C/C++ compiler are installed.
+echo 'Run this script as sudo...'
 
 # exit on error
 set -e
@@ -16,11 +12,11 @@ DESTINATION_PATH=/usr/local/bin
 
 VERSION=3.4
 
-# wget -O $INSTALLER_PATH/nnn-${VERSION}.tar.gz https://github.com/jarun/nnn/archive/v${VERSION}.tar.gz
+wget -O $INSTALLER_PATH/nnn-${VERSION}.tar.gz https://github.com/jarun/nnn/archive/v${VERSION}.tar.gz
 
-# cd /opt/installers/nnn && tar xfzv nnn-${VERSION}.tar.gz
+cd /opt/installers/nnn && tar xfzv nnn-${VERSION}.tar.gz
 
-# cd /opt/installers/nnn/nnn-${VERSION} && make
+cd /opt/installers/nnn/nnn-${VERSION} && make
 
 cp /opt/installers/nnn/nnn-${VERSION}/nnn $DESTINATION_PATH
 
