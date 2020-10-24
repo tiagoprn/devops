2c1b276b68de5890caf65e64602b49535e7aa210
tiago.paranhos <tiagoprn@gmail.com>
2020-10-24 09:55:38 -0300

    feat(shellscripts: (wip) nnn installer.

--- shellscripts/installers/install_nnn.sh
+++ shellscripts/installers/install_nnn.sh
@@ -12,12 +12,16 @@ set -e
 INSTALLER_PATH=/opt/installers/nnn
 mkdir -p $INSTALLER_PATH
 
+DESTINATION_PATH=/usr/local/bin
+
 VERSION=3.4
 
 # wget -O $INSTALLER_PATH/nnn-${VERSION}.tar.gz https://github.com/jarun/nnn/archive/v${VERSION}.tar.gz
 
 # cd /opt/installers/nnn && tar xfzv nnn-${VERSION}.tar.gz
 
-cd /opt/installers/nnn/nnn-${VERSION} && make
+# cd /opt/installers/nnn/nnn-${VERSION} && make
 
-echo "nnn sucessfully installed at ."
+cp /opt/installers/nnn/nnn-${VERSION}/nnn $DESTINATION_PATH
+
+echo "nnn sucessfully installed at $DESTINATION_PATH."
