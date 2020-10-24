8dafac71632ef8f0223882cfdbd1a76b23e62b69
tiago.paranhos <tiagoprn@gmail.com>
2020-10-24 10:29:38 -0300

    feat(shellscripts): fixing fzf installer.

--- shellscripts/installers/install_fzf.sh
+++ shellscripts/installers/install_fzf.sh
@@ -12,10 +12,10 @@ VERSION=0.23.1
 
 # https://github.com/junegunn/fzf/archive/0.23.1.tar.gz
 
-wget -O $INSTALLER_PATH/$VERSION.tar.gz https://github.com/junegunn/fzf/archive/$VERSION.tar.gz
+wget -O $INSTALLER_PATH/fzf-$VERSION.tar.gz https://github.com/junegunn/fzf/archive/$VERSION.tar.gz
 
-cd $INSTALLER_PATH && tar xfzv $VERSION.tar.gz
+cd $INSTALLER_PATH && tar xfzv fzf-$VERSION.tar.gz
 
-cd $INSTALLER_PATH/$VERSION && make && make install
+cd $INSTALLER_PATH/fzf-$VERSION && make && make install
 
 echo "fzf sucessfully installed."
