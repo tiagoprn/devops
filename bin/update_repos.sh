425f23ee579c4c4d935edd9fb44ff5e944faf85a
tiago.paranhos <tiagoprn@gmail.com>
2020-11-02 18:17:57 -0300

    feat: finished script to update all my repos at once.

--- bin/update_repos.sh
+++ bin/update_repos.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
+REPOS="/storage/src/devops /storage/src/dot_files /storage/src/bin_private /storage/docs/notes /storage/src/tiagoprnl /storage/src/gpg"
+UPDATE_BRANCH=master
+
 for repo in $REPOS; do
 	echo -e '-----'
-	echo " >>> Updating repository '$repo'..."
-	cd /storage/src/$repo && git fetch && git checkout $UPDATE_BRANCH && git pull
+	echo " >>> Updating repository '$repo' (branch $UPDATE_BRANCH)..."
+	cd $repo && git fetch && git checkout $UPDATE_BRANCH && git pull origin $UPDATE_BRANCH
 done
 
 echo -e '-----'
