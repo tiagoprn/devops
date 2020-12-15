9aad44e92248619149232487e0b105b899a9f5e0
tiago.paranhos <tiagoprn@gmail.com>
2020-12-15 07:52:43 -0300

    fix(rofi): fix to rofi script syntax.

--- rofi/snippets.sh
+++ rofi/snippets.sh
@@ -5,7 +5,7 @@ SNIPS=/storage/src/dot_files/text_snippets
 FILE=$(ls $SNIPS | rofi -dmenu)
 
 if [ -f $SNIPS/$FILE ]; then
-	DATA=$([ -x "$SNIPS/$FILE"] && bash "$SNIPS/$FILE" || head --bytes=-1 $SNIPS/$FILE)
+	DATA=$([ -x "$SNIPS/$FILE" ] && bash "$SNIPS/$FILE" || head --bytes=-1 $SNIPS/$FILE)
 	printf "$DATA" | xclip -selection clipboard
 	printf "$DATA" | xclip -selection primary
 	xdotool key shift+Insert
