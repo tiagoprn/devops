629af095cfa7db089ae6645b76b56a3b2fc6b4ef
tiago.paranhos <tiagoprn@gmail.com>
2021-01-07 20:03:06 -0300

    feat(utils): enhanced function to check process name anywhere on string (useful e.g. for python script)

--- shellscripts/utils/check_process_is_running.sh
+++ shellscripts/utils/check_process_is_running.sh
@@ -2,10 +2,11 @@
 
 _isRunning() {
 	## function to check if a process is alive and running:
-	ps -o comm= -C "$1" 2>/dev/null | grep -x "$1" >/dev/null 2>&1
+	ps -o args= -C "$1" 2>/dev/null | grep -x "$1" >/dev/null 2>&1
+	ps aux | grep "$1" 2>/dev/null | grep -v "grep" >/dev/null 2>&1
 }
 
-PROCESS_NAME=lxpanel
+PROCESS_NAME=lxterm
 
 ## Verbose solution
 
