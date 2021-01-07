c6af26407ce71925d1fee64230a319e6afcdf94b
tiago.paranhos <tiagoprn@gmail.com>
2021-01-06 21:32:32 -0300

    feat(scripts): changed old i3 scripts to now work on dwm.

--- bin/i3-personal-calendar.sh
+++ bin/i3-personal-calendar.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
-kitty --name personal-calendar --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml"
 
+OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
+if [ "$OS_NAME" == "raspbian" ]; then
+	TMUXP_BIN=/usr/local/bin/tmuxp
+	bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml";
+else
+	TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
+	kitty --name personal-calendar --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml"
+fi
--- bin/i3-vim.sh
+++ bin/i3-vim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# urxvt -name vim --hold -e bash -c 'vim /storage/docs/notes/personal/misc.md'
-TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
-kitty --name vim --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml"
 
+OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
+if [ "$OS_NAME" == "raspbian" ]; then
+	TMUXP_BIN=/usr/local/bin/tmuxp
+	bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml";
+else
+	TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
+	kitty --name vim --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/notes.yml"
+fi
