bde4ed7bdd1c941ecc5874ea2af12c1bf8e6f7bd
tiago.paranhos <tiagoprn@gmail.com>
2021-01-08 10:06:44 -0300

    feat: removing deduplication to ease detection of duplicated entries.

--- bin/dwm-print-all-mapped-keys.sh
+++ bin/dwm-print-all-mapped-keys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dwm-print-cheatsheet.sh | cut -d '=' -f 2 | cut -d '>' -f 2 | cut -d ' ' -f 2 | sort | uniq
+dwm-print-cheatsheet.sh | cut -d '=' -f 2 | cut -d '>' -f 2 | cut -d ' ' -f 2 | sort
