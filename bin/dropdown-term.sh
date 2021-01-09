c791e9a8afb867751fb077830a860af2f1031877
tiago.paranhos <tiagoprn@gmail.com>
2021-01-09 14:06:35 -0300

    feat: misc changes

--- bin/dropdown-term.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
-if [ "$OS_NAME" == "raspbian" ]; then
-    urxvt -hold -title dropdownterm -e bash -c "TERM=screen-256color /bin/bash";
-else
-    kitty --name dropdownterm --hold bash -c "TERM=screen-256color /bin/bash";
-fi
-
