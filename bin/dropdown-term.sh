74cede26305dcb8d8a74f01fda33b15eaf9c760f
tiago.paranhos <tiagoprn@gmail.com>
2021-01-08 23:51:11 -0300

    fix: urxvt params.

--- bin/dropdown-term.sh
+++ bin/dropdown-term.sh
@@ -2,7 +2,7 @@
 
 OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
 if [ "$OS_NAME" == "raspbian" ]; then
-    urxvt -name dropdownterm --hold -e bash -c "TERM=screen-256color /bin/bash";
+    urxvt -hold -title dropdownterm -e bash -c "TERM=screen-256color /bin/bash";
 else
     kitty --name dropdownterm --hold bash -c "TERM=screen-256color /bin/bash";
 fi
--- bin/i3-personal-calendar.sh
+++ bin/i3-personal-calendar.sh
@@ -3,7 +3,7 @@
 OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
 if [ "$OS_NAME" == "raspbian" ]; then
 	TMUXP_BIN=/usr/local/bin/tmuxp
-	urxvt -name personal-calendar --hold -e bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml";
+	urxvt -hold -title personalcalendar  -e bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml";
 else
 	TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
 	kitty --name personal-calendar --hold bash -c "TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/calendar.yml"
