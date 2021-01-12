b950efd65a57050bbfc337b4350bacc3cec91fe6
tiago.paranhos <tiagoprn@gmail.com>
2021-01-12 19:46:45 -0300

    feat: new scripts.

---
+++ bin/lock-terminal-for-input.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "type something to unlock. The screen will be resetted and cleared after finished."
+read key
+reset
+clear
--- bin/wal-reload-last-theme.sh
+++ bin/wal-reload-last-theme.sh
@@ -3,7 +3,6 @@
 if [ -x "$(command -v wal)" ]; then
     wal -R -n
 else
-    echo 'wal command is not installed. Nothing to do.'
+    echo "wal command is not installed. Nothing to do."
 fi
 
-exit 0
