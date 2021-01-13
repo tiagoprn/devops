fbf0e92572c5a154732f77b732674448d3ec3e7c
tiago.paranhos <tiagoprn@gmail.com>
2021-01-13 14:54:40 -0300

    feat(bin): keeping only the caps lock status indicator.

--- bin/keyboard-leds.sh
+++ bin/keyboard-leds.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]' | sed 's/,/ | /g' | sed 's/LOCK//g' | sed 's/ ://g' | sed 's/  / /g' | sed 's/ | /|/g' | sed 's/CAPS/CAP/g' | sed 's/SCROLL/SCR/g')
+STATUS=$(xset -q | grep Caps | sed 's/0[0-2]://g' | sed 's/off/off,/g' | sed 's/on/on,/g' |  sed 's/.$//' | tr -s ' ' | tr '[a-z]' '[A-Z]' | sed 's/,/ | /g' | sed 's/LOCK//g' | sed 's/ ://g' | sed 's/  / /g' | sed 's/ | /|/g' | sed 's/CAPS/CAP/g' | sed 's/SCROLL/SCR/g' | cut -d '|' -f 1)
 
-if [[ "$STATUS" == *"ON"* ]]; then
-    COLOR='#00FF00';
-else
-    COLOR='#FFFFFF';
-fi
-
-# To change color of the text on the block, the output must have three lines and the third one must
-# be a hexadecimal color.
 echo -e "$STATUS";
