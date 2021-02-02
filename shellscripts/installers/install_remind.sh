ce1029cdcc458a0bc431273d9255f82e0b5dfb23
tiago.paranhos <tiagoprn@gmail.com>
2021-02-02 08:49:16 -0300

    feat(installers): fix to remind install script.

--- shellscripts/installers/install_remind.sh
+++ shellscripts/installers/install_remind.sh
@@ -7,6 +7,7 @@ clear
 echo "This script is intended to be run as sudo...."
 sleep 5
 
+echo "Installing remind..."
 ROOT=/opt
 DOWNLOAD_DIR=$ROOT/installers/remind/src
 INSTALL_DIR=$ROOT/remind
@@ -16,4 +17,10 @@ wget https://dianne.skoll.ca/projects/remind/download/remind-$VERSION.tar.gz
 
 mkdir -p $INSTALL_DIR
 tar xfzv $DOWNLOAD_DIR/remind-$VERSION.tar.gz -C $INSTALL_DIR
-cd $INSTALL_DIR/remind-$VERSION && ./configure && make && make test && sudo make install
+cd $INSTALL_DIR/remind-$VERSION && ./configure && make & make install 
+# cp -farv $INSTALL_DIR/remind-$VERSION/src/remind /usr/bin
+echo "Finished installing remind." 
+echo "Installing telegram-send..."
+pip3 install telegram-send
+echo "Finished installing telegram-send." 
+echo "DONE."
