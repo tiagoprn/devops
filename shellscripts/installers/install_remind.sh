1d0468eb5b08e4088b26a08408dd8d5146d3a301
tiago.paranhos <tiagoprn@gmail.com>
2021-02-02 08:22:47 -0300

    feat(installers): updated shellscript after first test install on centos.

--- shellscripts/installers/install_remind.sh
+++ shellscripts/installers/install_remind.sh
@@ -3,6 +3,10 @@
 # HOMEPAGE: https://dianne.skoll.ca/projects/remind/
 set -e
 
+clear
+echo "This script is intended to be run as sudo...."
+sleep 5
+
 ROOT=/opt
 DOWNLOAD_DIR=$ROOT/installers/remind/src
 INSTALL_DIR=$ROOT/remind
@@ -10,5 +14,6 @@ VERSION=03.03.05
 mkdir -p $DOWNLOAD_DIR && cd $DOWNLOAD_DIR
 wget https://dianne.skoll.ca/projects/remind/download/remind-$VERSION.tar.gz
 
-tar xfzv $DOWNLOAD_DIR/remind-$VERSION.tar.gz $INSTALL_DIR
+mkdir -p $INSTALL_DIR
+tar xfzv $DOWNLOAD_DIR/remind-$VERSION.tar.gz -C $INSTALL_DIR
 cd $INSTALL_DIR/remind-$VERSION && ./configure && make && make test && sudo make install
