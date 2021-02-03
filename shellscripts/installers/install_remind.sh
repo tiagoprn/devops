16c88610f7814d2155375adbec1fc90868db2859
tiago.paranhos <tiagoprn@gmail.com>
2021-02-03 07:21:30 -0300

    refac: better general naming

--- shellscripts/installers/install_remind.sh
+++ shellscripts/installers/install_remind.sh
@@ -10,17 +10,17 @@ sleep 5
 echo "Installing remind..."
 ROOT=/opt
 DOWNLOAD_DIR=$ROOT/installers/remind/src
-INSTALL_DIR=$ROOT/remind
+COMPILE_DIR=$ROOT/src/remind
 VERSION=03.03.05
 mkdir -p $DOWNLOAD_DIR && cd $DOWNLOAD_DIR
 wget https://dianne.skoll.ca/projects/remind/download/remind-$VERSION.tar.gz
 
-mkdir -p $INSTALL_DIR
-tar xfzv $DOWNLOAD_DIR/remind-$VERSION.tar.gz -C $INSTALL_DIR
-cd $INSTALL_DIR/remind-$VERSION && ./configure && make & make install 
-# cp -farv $INSTALL_DIR/remind-$VERSION/src/remind /usr/bin
-echo "Finished installing remind." 
+mkdir -p $COMPILE_DIR
+tar xfzv $DOWNLOAD_DIR/remind-$VERSION.tar.gz -C $COMPILE_DIR
+cd $COMPILE_DIR/remind-$VERSION && ./configure && make & make install
+# cp -farv $COMPILE_DIR/remind-$VERSION/src/remind /usr/bin
+echo "Finished installing remind."
 echo "Installing telegram-send..."
 pip3 install telegram-send
-echo "Finished installing telegram-send." 
+echo "Finished installing telegram-send."
 echo "DONE."
