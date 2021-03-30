8331304c8dd51ceb43ba427faec2e196cd9cf86c
tiago.paranhos <tiagoprn@gmail.com>
2021-03-30 06:50:11 -0300

    feat(bin): added dmenu scripts aggregator.

---
+++ bin/dmlauncher.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DMMENU_SCRIPTS_PATH=$HOME/apps/scripts/bin
+
+
+# An array of options
+declare -A options
+options[kill]="dmkill.sh"
+options[search]="dmsearch.sh"
+options[hex_color]="dmcolors.sh"
+
+# Picking a script
+# shellcheck disable=SC2154
+while [ -z "$script" ]; do
+    script=$(printf '%s\n' "${!options[@]}" | sort | dmenu -i -l 20 -p 'Choose script:') "$@" || exit
+    chosen_script="${options["${script}"]}" || exit
+done
+
+/bin/bash "$DMMENU_SCRIPTS_PATH/$chosen_script"
