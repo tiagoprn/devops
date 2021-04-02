115284a5b3f6505fdfbf2b6caa6c743a3e6edbdc
tiago.paranhos <tiagoprn@gmail.com>
2021-04-02 10:18:36 -0300

    feat(bin/tmuxp): updated tmux session and urxvt trigger script.

--- bin/urxvt-terminal.sh
+++ bin/urxvt-terminal.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-notify-send 'Opening bigger urxvt terminal...'
-urxvt -name presentation -cd /storage/docs/notes
+
+# notify-send 'Opening bigger urxvt terminal...'
+# urxvt -name presentation -cd /storage/docs/notes
+
+WINDOW_NAME=dropdownterm
+WIDTH=120
+HEIGHT=30
+
+urxvt -name $WINDOW_NAME -title $WINDOW_NAME -geometry ${WIDTH}x${HEIGHT}
+
