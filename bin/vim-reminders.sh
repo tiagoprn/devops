157ad4e1688b498cab37d724d44a08b03b0b8038
tiago.paranhos <tiagoprn@gmail.com>
2021-04-02 11:55:12 -0300

    feat(bin): removed duplicated scripts.

--- bin/vim-reminders.sh
+++ bin/vim-reminders.sh
@@ -3,8 +3,7 @@
 OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
 if [ "$OS_NAME" == "raspbian" ]; then
 	TMUXP_BIN=/usr/local/bin/tmuxp
-	TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/reminders.yml
 else
 	TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
-	TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/reminders.yml
 fi
+TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/reminders.yml
