8328916b4c1e62145dd25b84108eb4a602a15cd8
tiago.paranhos <tiagoprn@gmail.com>
2021-04-02 10:12:51 -0300

    feat(bin): added vim reminders script.

---
+++ bin/vim-reminders.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OS_NAME=$(cat /etc/os-release  | grep '^ID=' | cut -d '=' -f 2)
+if [ "$OS_NAME" == "raspbian" ]; then
+	TMUXP_BIN=/usr/local/bin/tmuxp
+	TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/reminders.yml
+else
+	TMUXP_BIN=$HOME/.pyenv/versions/core-utils/bin/tmuxp
+	TERM=screen-256color $TMUXP_BIN load /storage/src/devops/tmuxp/reminders.yml
+fi
