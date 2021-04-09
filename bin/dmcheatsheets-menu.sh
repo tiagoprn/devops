54feae7b3856edca1eefd413e10bdfc2b2428685
tiago.paranhos <tiagoprn@gmail.com>
2021-04-09 06:29:19 -0300

    feat(bin): updated cheatsheets script to include vim.

--- bin/dmcheatsheets-menu.sh
+++ bin/dmcheatsheets-menu.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-DMMENU_SCRIPTS_PATH=$HOME/apps/scripts/bin
+DMENU_SCRIPTS_PATH=$HOME/apps/scripts/bin
+ROFI_SCRIPTS_PATH=$HOME/apps/scripts/rofi
+ROFI_RUN_PREFIX="python3 $ROFI_SCRIPTS_PATH"
 
 # An array of options
 declare -A options
-options[dwm]="dwm-dmenu-cheatsheet.sh"
+options[dwm]="$DMENU_SCRIPTS_PATH/dwm-dmenu-cheatsheet.sh"
+options[vim]="$ROFI_RUN_PREFIX/vim_shortcuts.py"
 
 # Picking a script
 # shellcheck disable=SC2154
@@ -13,4 +16,4 @@ while [ -z "$script" ]; do
     chosen_script="${options["${script}"]}" || exit
 done
 
-/bin/bash "$DMMENU_SCRIPTS_PATH/$chosen_script"
+/bin/bash -c "$chosen_script"
