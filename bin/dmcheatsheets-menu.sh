14dac698e0669b550bbdb51cb8876b85d42be64a
tiago.paranhos <tiagoprn@gmail.com>
2021-04-09 06:18:47 -0300

    feat(bin): refactoring shell scripts.

---
+++ bin/dmcheatsheets-menu.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DMMENU_SCRIPTS_PATH=$HOME/apps/scripts/bin
+
+# An array of options
+declare -A options
+options[dwm]="dwm-dmenu-cheatsheet.sh"
+
+# Picking a script
+# shellcheck disable=SC2154
+while [ -z "$script" ]; do
+    script=$(printf '%s\n' "${!options[@]}" | sort | dmenu -fn Terminus:size=14 -c -bw 2 -l 20 -p 'Choose a cheatsheet to view:') "$@" || exit
+    chosen_script="${options["${script}"]}" || exit
+done
+
+/bin/bash "$DMMENU_SCRIPTS_PATH/$chosen_script"
--- rofi/dwm_cheatsheet.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dwm-print-cheatsheet.sh | sed 's/&/&amp;/g' |  rofi -dmenu -markup -markup-rows -fullscreen --no-case-sensitive -p "Filter a dwm shorcut"
