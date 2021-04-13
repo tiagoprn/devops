0424335caf108ddbeb1aadfa498eb45b17db6de0
tiago.paranhos <tiagoprn@gmail.com>
2021-04-13 06:56:36 -0300

    feat(shellscripts): kvm: supporting ubuntu and centos as distros.

--- shellscripts/kvm/create-vm.sh
+++ shellscripts/kvm/create-vm.sh
@@ -1,13 +1,14 @@
-#!/bin/bash 
+#!/bin/bash
 
 # e.g.
-SYNTAX="./create-vm.sh -n [VM_NAME] -b [BRIDGE_NAME] -r [RAM] -c [NUMBER_OF_CPUS]"
-HELP="./create-vm.sh -n centos7-02 -b br1 -r 1024 -c 1"
+SYNTAX="./create-vm.sh -d [DISTRO_NAME] -n [VM_NAME] -b [BRIDGE_NAME] -r [RAM] -c [NUMBER_OF_CPUS]"
+HELP="./create-vm.sh -d ubuntu18 -n centos7-02 -b br1 -r 1024 -c 1"
 
 while getopts n:b:r:c: option
 do
   case "${option}"
   in
+  d) DISTRO_NAME=${OPTARG};;
   n) VM_NAME=${OPTARG};;
   b) BRIDGE_NAME=${OPTARG};;
   r) RAM=${OPTARG};;
@@ -16,6 +17,12 @@ do
 done
 
 echo 'Parsing script parameters...'
+
+if [[ -z $DISTRO_NAME ]]; then
+    DISTRO_NAME="ubuntu18"
+    printf "Missing -d parameter. I will use $DISTRO_NAME.\n";
+fi
+
 if [[ -z $VM_NAME ]]; then
     printf "Missing -n parameter. Syntax: \n\n$SYNTAX\n\nE.g.: \n\n$HELP\n";
     exit 1
@@ -44,8 +51,13 @@ then
     exit 3
 fi
 
-echo 'Downloading the CentOS 7 cloud image...';
-bash ./download_centos7_cloud_image.sh;
+if [ $DISTRO_NAME = "ubuntu18" ]; then
+	echo 'Downloading the Ubuntu 18 cloud image...';
+	bash ./download_ubuntu18_cloud_image.sh;
+else
+	echo 'Downloading the CentOS 7 cloud image...';
+	bash ./download_centos7_cloud_image.sh;
+fi
 
 echo 'Generating the cloud init iso...';
 bash ./generate_cloud_init_iso.sh $VM_NAME;
@@ -55,10 +67,17 @@ sudo mkdir -p /kvm/templates
 sudo mkdir -p /kvm/images
 sudo mkdir -p /kvm/iso
 sudo chown -R nobody.nobody /kvm && sudo chmod -R 777 /kvm/
-cp -farv ~/distros/images/CentOS-7-x86_64-GenericCloud.template.qcow2 /kvm/templates
+
+if [ $DISTRO_NAME = "ubuntu18" ]; then
+	cp -farv ~/distros/images/ubuntu-1804-i386.template.qcow2 /kvm/templates
+	cp -farv /kvm/templates/ubuntu-1804-i386.template.qcow2 $VM_IMAGE_FILE
+else
+	cp -farv ~/distros/images/CentOS-7-x86_64-GenericCloud.template.qcow2 /kvm/templates
+	cp -farv /kvm/templates/CentOS-7-x86_64-GenericCloud.template.qcow2 $VM_IMAGE_FILE
+fi
 
 echo 'Creating a new VM from the image...'
-cp -farv /kvm/templates/CentOS-7-x86_64-GenericCloud.template.qcow2 $VM_IMAGE_FILE 
+
 ./generate_cloud_init_iso.sh $VM_NAME
 cp -farv ~/distros/images/$VM_NAME-cloud-init-data.iso /kvm/iso
 sudo virt-install --import --name $VM_NAME --ram $RAM --vcpus $CPUS --cpu host --disk /kvm/images/$VM_NAME.qcow2,format=qcow2,bus=virtio --disk /kvm/iso/$VM_NAME-cloud-init-data.iso,device=cdrom --network bridge=$BRIDGE_NAME,model=virtio --os-type=linux --os-variant=rhel7 --noautoconsole
@@ -72,7 +91,7 @@ echo '==> The machine is named' $VM_NAME', has a network bridge at '$BRIDGE_NAME
 bash ./get_domain_ip.sh $VM_NAME
 
 echo 'Ejecting the iso created by cloud-init - final time...' # this updates the registry that generates the dumpxml command, so the backup does not need the iso anymore.
-sudo virsh change-media $VM_NAME hda --eject 
+sudo virsh change-media $VM_NAME hda --eject
 echo 'Virtual disks attached to the VM:'
 sudo virsh domblklist $VM_NAME
 
