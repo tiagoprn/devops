08f9231ce412ce66cfeded238d61d20743721b78
tiago.paranhos <tiagoprn@gmail.com>
2021-06-09 12:01:28 -0300

    feat(bin): alacritty cheatsheets menu scripts.

--- bin/dmcheatsheets-menu.sh
+++ bin/dmcheatsheets-menu.sh
@@ -9,13 +9,14 @@ declare -A options
 options[dwm]="$DMENU_SCRIPTS_PATH/dwm-dmenu-cheatsheet.sh"
 options[qutebrowser]="$DMENU_SCRIPTS_PATH/qutebrowser-dmenu-cheatsheet.sh"
 options[st]="$DMENU_SCRIPTS_PATH/st-dmenu-cheatsheet.sh"
+options[alacritty]="$DMENU_SCRIPTS_PATH/alacritty-dmenu-cheatsheet.sh"
 options[vim]="$ROFI_RUN_PREFIX/vim_shortcuts.py -f vim"
 options[neovim]="$ROFI_RUN_PREFIX/vim_shortcuts.py -f neovim"
 
 # Picking a script
 # shellcheck disable=SC2154
 while [ -z "$script" ]; do
-    script=$(printf '%s\n' "${!options[@]}" | sort | dmenu -fn Terminus:size=14 -c -bw 2 -l 20 -p 'Choose a cheatsheet to view:') "$@" || exit
+    script=$(printf '%s\n' "${!options[@]}" | sort | dmenu -fn 'Jetbrains Mono:size=14' -c -bw 2 -l 20 -p 'Choose a cheatsheet to view:') "$@" || exit
     chosen_script="${options["${script}"]}" || exit
 done
 
