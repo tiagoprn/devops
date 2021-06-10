a90f842983f055929a52da1d448130d93de6fda2
tiago.paranhos <tiagoprn@gmail.com>
2021-06-10 16:10:32 -0300

    feat(bin): updated the scripts

--- bin/terminal-dropdownterm.sh
+++ bin/terminal-dropdownterm.sh
@@ -2,7 +2,7 @@
 DISTRO=$(cat /etc/os-release | grep -e '^ID=' | cut -d = -f 2)
 
 if [[ $DISTRO == "raspbian" ]]; then
-  st -n dropdownterm -c Dropdownterm -g 120x45
+  st -n dropdownterm -c Dropdownterm -g 120x45 -e bash -c 'TERM=screen-256color ~/apps/scripts/bin/start_random_tmux_session_name.sh'
 else
   alacritty --class dropdownterm --title dropdownterm --hold -e tmux
 fi
--- bin/terminal.sh
+++ bin/terminal.sh
@@ -2,7 +2,7 @@
 DISTRO=$(cat /etc/os-release | grep -e '^ID=' | cut -d = -f 2)
 
 if [[ $DISTRO == "raspbian" ]]; then
-  st
+  st -e bash -c 'TERM=screen-256color ~/apps/scripts/bin/start_random_tmux_session_name.sh'
 else
   alacritty
 fi
