5cfc4146d8784079e5699bd4db511c0e07d88636
tiago.paranhos <tiagoprn@gmail.com>
2021-06-10 15:40:18 -0300

    feat(bin): updated script

--- bin/terminal-nnn.sh
+++ bin/terminal-nnn.sh
@@ -2,7 +2,7 @@
 DISTRO=$(cat /etc/os-release | grep -e '^ID=' | cut -d = -f 2)
 
 if [[ $DISTRO == "raspbian" ]]; then
-  urxvt -name nnn -title nnn --hold -e nnn
+  st -n nnn -t nnn -e nnn
 else
   alacritty --class nnn --title nnn --hold -e nnn
 fi
