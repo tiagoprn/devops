11a7fbba8ccd982d243b4873689fb2492e77d4bb
tiago.paranhos <tiagoprn@gmail.com>
2021-06-19 17:14:07 -0300

    feat(bin): updated script.

--- bin/star-trek-stardate.sh
+++ bin/star-trek-stardate.sh
@@ -3,6 +3,5 @@
 STAR_DATE=$(curl -s https://www.stoacademy.com/tools/stardate.php | grep 'Stardate' | grep 'div' | cut -d '>' -f 3 | cut -d '<' -f 1 | sed 's/S/s/g' | sed 's/ /: /g')
 EARTH_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 
-echo "EARTH DATE: $EARTH_DATE"
-echo "$STAR_DATE"
+echo "$STAR_DATE (earth: $EARTH_DATE)"
 
