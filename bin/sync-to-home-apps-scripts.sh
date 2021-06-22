5765b074bab518b78f36d66d487936e1890abc97
tiago.paranhos <tiagoprn@gmail.com>
2021-06-22 17:45:29 -0300

    feat: fixed to reproduce a new dwm build.

--- bin/sync-to-home-apps-scripts.sh
+++ bin/sync-to-home-apps-scripts.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp -farv /storage/src/devops/bin/* $HOME/apps/scripts/bin
+cp -farv /storage/src/devops/shellscripts/statusbar $HOME/apps/scripts/bin
 cp -farv /storage/src/devops/rofi/* $HOME/apps/scripts/rofi
