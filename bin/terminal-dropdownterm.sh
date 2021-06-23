aeddc794e604ec301a6cf03ecb75665dacffd3b4
tiago.paranhos <tiagoprn@gmail.com>
2021-06-22 22:44:39 -0300

    feat(bin): working dropdownterm.

--- bin/terminal-dropdownterm.sh
+++ bin/terminal-dropdownterm.sh
@@ -4,5 +4,17 @@ DISTRO=$(cat /etc/os-release | grep -e '^ID=' | cut -d = -f 2)
 if [[ $DISTRO == "raspbian" ]]; then
   nohup st -n dropdownterm -c Dropdownterm -g 120x45 -e bash -c 'TERM=screen-256color ~/apps/scripts/bin/start_random_tmux_session_name.sh'
 else
-  alacritty --class dropdownterm --title dropdownterm --hold -e tmux
+  TMUXP="$HOME/.pyenv/versions/core-utils/bin/tmuxp"
+
+  if [ -z "$TMUXP" ]; then
+    TMUXP='tmuxp'
+
+    if [ -z "$TMUXP" ]; then
+      echo missing tmuxp, please install to proceed.
+      exit 1
+    fi
+
+  fi
+
+  alacritty --class dropdownterm -e bash -c "$TMUXP load /storage/src/devops/tmuxp/notes-and-reminders.yml" --hold
 fi
