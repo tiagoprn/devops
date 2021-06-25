58c2555ad42557ba291b3173f72f9f172f78e24e
tiago.paranhos <tiagoprn@gmail.com>
2021-06-25 16:32:32 -0300

    feat(rofi): updated script.

--- rofi/snippets.sh
+++ rofi/snippets.sh
@@ -2,7 +2,7 @@
 
 SNIPS=/storage/src/dot_files/text_snippets
 
-FILE=$(ls $SNIPS | rofi -dmenu)
+FILE=$(ls $SNIPS | rofi -dmenu -p 'Select a snippet to be pasted on your current cursor position')
 
 if [ -f $SNIPS/$FILE ]; then
 	DATA=$([ -x "$SNIPS/$FILE" ] && bash "$SNIPS/$FILE" || head --bytes=-1 $SNIPS/$FILE)
