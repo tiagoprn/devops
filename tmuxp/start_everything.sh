8e9956a9513ba90d256c84027205b7311db4d1b6
tiago.paranhos <tiagoprn@gmail.com>
2021-07-22 18:54:57 -0300

    feat(tmuxp): tmux sesions cleanup

--- tmuxp/start_everything.sh
+++ tmuxp/start_everything.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 tmuxp load -d /storage/src/devops/tmuxp/logs.yml
 tmuxp load -d /storage/src/devops/tmuxp/ansible.yml
-tmuxp load -d /storage/src/devops/tmuxp/tiagopr.nl.yml
 tmuxp load -d /storage/src/devops/tmuxp/home.yml
-tmuxp load -d /storage/src/devops/tmuxp/dev.yml
-tmuxp load -d /storage/src/devops/tmuxp/core-utils.yml
-tmuxp load -d /storage/src/devops/tmuxp/db-utils.yml
-tmuxp load -d /storage/src/devops/tmuxp/pyenv-python-project.yml
