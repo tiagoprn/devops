5d2b404540858ab9bad17ce0842316a77bd90af1
tiago.paranhos <tiagoprn@gmail.com>
2021-11-18 09:29:15 -0300

    feat(shellscripts): updated pyenv install script.

--- shellscripts/installers/install_or_upgrade_pyenv.sh
+++ shellscripts/installers/install_or_upgrade_pyenv.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
 
-PYENV_ROOT="$HOME/.pyenv"
-PYENV_BIN=$PYENV_ROOT/bin/pyenv
+# Created with instructions from:
+# https://github.com/pyenv/pyenv#basic-github-checkout
+# https://github.com/pyenv/pyenv-virtualenv
+# IMPORTANT: if anything goes wrong, make sure to check those links above for updated instructions
+#            (specially if pyenv virtualenvs stop working - it will probably be related to ~/.profile and/or ~/.bashrc).
+
+BASH_PROFILE_PATH=$HOME/.profile
+BASH_RC_PATH=$HOME/.bashrc
+
+PYENV_ROOT_FOLDER="$HOME/.pyenv"
+PYENV_BIN=$PYENV_ROOT_FOLDER/bin/pyenv
 
 if [ -f $PYENV_BIN ];
 then
-    echo "UPGRADING pyenv..."
-    echo "Retrieving available tags:"
-    cd $PYENV_ROOT && git fetch
-    echo "Choose the desired tag with the pyenv version:"
-    TAG=$(git tag | grep "v[1-9]\." | fzf)
-    git checkout $TAG
-    echo "FINISHED."
+
+	echo "UPGRADING pyenv (from master branch)..."
+	cd $PYENV_ROOT_FOLDER && git fetch && git pull && git checkout master
+
+	echo "UPGRADING pyenv-virtualenv (from master branch)..."
+	cd $PYENV_ROOT_FOLDER/plugins/pyenv-virtualenv && git fetch && git pull && git checkout master
+
+	echo "FINISHED. If your pyenv/pyenv-virtualenv stops working, delete $PYENV_ROOT_FOLDER and run this script again."
+
 else
-    echo "INSTALLING pyenv..."
-    git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
-    git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
-    echo "If you installed from other source than this repository, apply the changes from my files
-    .bash_environment (PYENV named environment variables) and .bashrc (pyenv initialization) to your
-    .bashrc."
-    echo "FINISHED. Quit this shell or start another one now."
+
+	sleep 5
+	echo "INSTALLING pyenv..."
+	git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT_FOLDER
+	git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT_FOLDER/plugins/pyenv-virtualenv
+
+	echo "The steps below will change $BASH_RC_PATH and $BASH_PROFILE_PATH. I will backup their original contents to make sure you can revert their contents if you need it..."
+	cp -fav "$BASH_PROFILE_PATH" "$BASH_PROFILE_PATH.ORIG"
+	cp -fav "$BASH_RC_PATH" "$BASH_RC_PATH.ORIG"
+	echo "Original bash config files successfully backed up."
+
+	echo "Updating $BASH_PROFILE_PATH and $BASH_RC_PATH configuration, so that pyenv/pyenv-virtualenv work..."
+	# the sed invocation inserts the lines at the start of the file after any initial comment lines
+	sed -Ei -e '/^([^#]|$)/ {a \
+		export PYENV_ROOT="$HOME/.pyenv"
+	a \
+		export PATH="$PYENV_ROOT/bin:$PATH"
+	a \
+		' -e ':a' -e '$!{n;ba};}' $BASH_PROFILE_PATH
+		echo 'eval "$(pyenv init --path)"' >> $BASH_PROFILE_PATH
+		echo 'eval "$(pyenv init -)"' >> $BASH_RC_PATH
+	echo 'eval "$(pyenv virtualenv-init -)"' >> $BASH_RC_PATH
+
+	echo "Open $BASH_RC_PATH and $BASH_PROFILE_PATH and make sure there are no duplicate entries related to pyenv init/pyenv-virtualenv init on the files."
+	sleep 5
+	echo "FINISHED. Quit this shell or start another one now."
+
 fi
