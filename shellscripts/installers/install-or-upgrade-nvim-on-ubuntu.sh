6b13406970738423bdada8600af3da1231e5af8a
tiago.paranhos <tiagoprn@gmail.com>
2021-12-14 16:52:14 -0300

    feat(shellscripts): new neovim install or update script.

---
+++ shellscripts/installers/install-or-upgrade-nvim-on-ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DOWNLOAD_PATH='/opt/nvim'
+APP_IMAGE_NAME='nvim.appimage'
+BACKUP_PATH="$DOWNLOAD_PATH/OLD/$APP_IMAGE_NAME.bkp.$(date +%Y%m%d-%H%M%S-%N)"
+
+sudo mkdir -p $DOWNLOAD_PATH/OLD
+
+echo -e 'Current version:'
+nvim --version || true
+
+echo -e 'Backing up current version...'
+sudo cp -farv "$DOWNLOAD_PATH/$APP_IMAGE_NAME" "$BACKUP_PATH" || true
+
+echo -e 'Closing all nvim running instances in 5 seconds...'
+sleep 5
+sudo pkill nvim
+
+echo -e "Downloading into '$DOWNLOAD_PATH/$APP_IMAGE_NAME'..."
+sudo curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o "$DOWNLOAD_PATH/$APP_IMAGE_NAME"
+
+echo -e 'Fixing permissions...'
+sudo chmod 777 $DOWNLOAD_PATH/$APP_IMAGE_NAME
+
+echo -e 'Successfully installed. New version:'
+nvim --version
+
+echo -e "If needed, the old nvim app image is available at $BACKUP_PATH."
