54f1a78160fe01e33424a8e7339188a497edbf86
tiago.paranhos <tiagoprn@gmail.com>
2022-04-18 20:12:27 -0300

    fix: another update to pyenv configuration.

--- shellscripts/installers/install_or_upgrade_pyenv.sh
+++ shellscripts/installers/install_or_upgrade_pyenv.sh
@@ -3,19 +3,22 @@
 # Created with instructions from:
 # https://github.com/pyenv/pyenv#basic-github-checkout
 # https://github.com/pyenv/pyenv-virtualenv
+# https://github.com/pyenv/pyenv-installer/issues/112#issuecomment-971964711
 # IMPORTANT: if anything goes wrong, make sure to check those links above for updated instructions
-#            (specially if pyenv virtualenvs stop working - it will probably be related to ~/.profile and/or ~/.bashrc).
+#            (specially if pyenv virtualenvs stop working - it will probably be related to ~/.bashrc).
 
 # About pyenv and shims:
 # https://mungingdata.com/python/how-pyenv-works-shims/?utm_source=pocket_mylist
 # https://stackoverflow.com/questions/2116142/what-is-a-shim?ref=hackernoon.com
 
 
-BASH_PROFILE_PATH=$HOME/.profile
 BASH_RC_PATH=$HOME/.bashrc
 
 PYENV_ROOT_FOLDER="$HOME/.pyenv"
 PYENV_BIN=$PYENV_ROOT_FOLDER/bin/pyenv
+
+BACKUP_FILE_SUFFIX=".BACKUP.$(date +%Y%m%d-%H%M%S-%N)"
+
 
 if [ -f $PYENV_BIN ];
 then
@@ -29,8 +32,8 @@ then
 	echo "FINISHED. If your pyenv/pyenv-virtualenv stops working, delete $PYENV_ROOT_FOLDER and run this script again."
 
 else
-	echo "Make sure you have the following packages installed on ubuntu or debian-base distros: \n"
-	echo "\t sudo apt install -y git build-essential python3-dev libssl-dev libbz2-dev libreadline-dev sqlite3 vim libsqlite3-dev"
+	echo -e "Make sure you have the following packages installed on ubuntu or debian-base distros: \n"
+	echo -e "\t sudo apt install -y git build-essential python3-dev libssl-dev libbz2-dev libreadline-dev sqlite3 vim libsqlite3-dev"
 
 	sleep 5
 
@@ -39,25 +42,20 @@ else
 	git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT_FOLDER
 	git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT_FOLDER/plugins/pyenv-virtualenv
 
-	echo "The steps below will change $BASH_RC_PATH and $BASH_PROFILE_PATH. I will backup their original contents to make sure you can revert their contents if you need it..."
-	cp -fav "$BASH_PROFILE_PATH" "$BASH_PROFILE_PATH.ORIG"
-	cp -fav "$BASH_RC_PATH" "$BASH_RC_PATH.ORIG"
-	echo "Original bash config files successfully backed up."
+	echo "The steps below will change $BASH_RC_PATH. I will backup its' original contents to make sure you can revert their contents if you need it..."
+	cp -fav "$BASH_RC_PATH" "$BASH_RC_PATH.$BACKUP_FILE_SUFFIX"
+	echo "Original bash config file successfully backed up at: $BASH_RC_PATH.$BACKUP_FILE_SUFFIX"
 
-	echo "Updating $BASH_PROFILE_PATH and $BASH_RC_PATH configuration, so that pyenv/pyenv-virtualenv work..."
-	# the sed invocation inserts the lines at the start of the file after any initial comment lines
-	sed -Ei -e '/^([^#]|$)/ {a \
-		export PYENV_ROOT="$HOME/.pyenv"
-	a \
-		export PATH="$PYENV_ROOT/bin:$PATH"
-	a \
-		' -e ':a' -e '$!{n;ba};}' $BASH_PROFILE_PATH
-		echo 'eval "$(pyenv init --path)"' >> $BASH_PROFILE_PATH
-		echo 'eval "$(pyenv init -)"' >> $BASH_RC_PATH
+	echo "Updating $BASH_RC_PATH, so that pyenv/pyenv-virtualenv work..."
+
+	echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $BASH_RC_PATH
+	echo 'export PATH="$PATH:$PYENV_ROOT/bin"' >> $BASH_RC_PATH
+	echo 'eval "$(pyenv init --path)"' >> $BASH_RC_PATH
+	echo 'eval "$(pyenv init -)"' >> $BASH_RC_PATH
 	echo 'eval "$(pyenv virtualenv-init -)"' >> $BASH_RC_PATH
 
-	echo "Open $BASH_RC_PATH and $BASH_PROFILE_PATH and make sure there are no duplicate entries related to pyenv init/pyenv-virtualenv init on the files."
+
+	echo "Open $BASH_RC_PATH and make sure there are no duplicate entries related to pyenv init/pyenv-virtualenv init on the files."
 	sleep 5
 	echo "FINISHED. Quit this shell or start another one now."
-
 fi
