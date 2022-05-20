499735bda519a6da1136539a985713294c7bc6f7
tiago.paranhos <tiagoprn@gmail.com>
2022-05-20 08:15:24 -0300

    feat(bin): updated sync-repos script.

--- bin/sync-repos.sh
+++ bin/sync-repos.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-REPOS="/storage/src/devops /storage/src/dot_files /storage/src/bin_private /storage/docs/notes /storage/src/tiagoprnl /storage/src/gpg /storage/src/reminders /storage/docs/fleeting-notes /storage/src/iac /storage/src/pass /storage/src/code-insights"
+REPOS="/storage/src/devops /storage/src/dot_files /storage/src/bin_private /storage/docs/notes /storage/src/tiagoprnl /storage/src/gpg /storage/src/reminders /storage/docs/fleeting-notes /storage/src/iac $HOME/.password-store /storage/src/code-insights"
 
-UPDATE_BRANCH=master
 
 for repo in $REPOS; do
 	if [ -d "$repo" ]; then
 		echo -e '-----'
-		echo " >>> Updating repository '$repo' (branch $UPDATE_BRANCH)..."
-		cd $repo && git fetch && git checkout $UPDATE_BRANCH && git pull origin $UPDATE_BRANCH
+		cd "$repo" && \
+			UPDATE_BRANCH=$(git branch | grep -e '^*' | cut -d '*' -f 2) && \
+			echo " >>> Updating repository '$repo' (branch $UPDATE_BRANCH)..." && \
+			git fetch && \
+			git pull origin $UPDATE_BRANCH
 	else
 		echo -e '-----'
-		echo "Repository not checked into "$repo", moving on to the next..."
+		echo "Repository not checked into '$repo', moving on to the next..."
 	fi
 done
 
