4d54e461949cd5a2c1de14619c6eef7d1ed13ee6
tiago.paranhos <tiagoprn@gmail.com>
2022-05-25 17:02:53 -0300

    feat(bin): updated bspwm-toggle-visibility-new.sh.

--- bin/flashcard.sh
+++ bin/flashcard.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-urxvt --hold --title flashcard -e /bin/bash -c 'fortune -u -e -c /storage/src/fortunes/ | tail +3 | cowsay -b -f dragon -W 80'
+_isRunning() {
+	## function to check if a process is alive and running:
+	ps -o args= -C "$1" 2>/dev/null | grep -x "$1" >/dev/null 2>&1
+	ps aux | grep "$1" 2>/dev/null | grep -v "grep" >/dev/null 2>&1
+}
+
+_isRunning urxvt || urxvt --hold --title flashcard -e /bin/bash -c 'fortune -u -e -c /storage/src/fortunes/ | tail +3 | cowsay -b -f dragon -W 80' &
