9ac201edbbe17e6f31df3709bb553ceab042a6f7
tiago.paranhos <tiagoprn@gmail.com>
2022-05-25 17:18:57 -0300

    feat: updated scripts.

--- bin/bspwm-toggle-visibility-new.sh
+++ bin/bspwm-toggle-visibility-new.sh
@@ -2,22 +2,23 @@
 
 # reference: https://my-take-on.tech/2020/07/03/some-tricks-for-sxhkd-and-bspwm/#easily-order-windows
 
-# In short, this script takes a process name (which is a class name or instance name, obtainable by xprop) and searches for it. Then, it applies a rule based on its status.
-
 window_id=${1}
 command=${2}
 
 echo "window_id: ${window_id}"
 echo "command: ${command}"
 
-# Get id of process by class name and then fallback to instance name
 id=$(wmctrl -l | grep $window_id | awk '{print $1}')
 
 echo "Looking for window $id ..."
 
 if [ -z "${id}" ]; then
     ${command} && sleep 0.2
+
+    # If the command below does not return any window ID, I must
+    # increase the value of the sleep command above.
     new_id=$(wmctrl -l | grep $window_id | awk '{print $1}')
+
     bspc node "$new_id" --focus -t floating
     echo "Window $new_id focused."
 else
--- bin/flashcard.sh
+++ bin/flashcard.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-_isRunning() {
-	## function to check if a process is alive and running:
-	ps -o args= -C "$1" 2>/dev/null | grep -x "$1" >/dev/null 2>&1
-	ps aux | grep "$1" 2>/dev/null | grep -v "grep" >/dev/null 2>&1
-}
-
-_isRunning urxvt || urxvt --hold --title flashcard -e /bin/bash -c 'fortune -u -e -c /storage/src/fortunes/ | tail +3 | cowsay -b -f dragon -W 80' &
+urxvt --hold --title flashcard -e /bin/bash -c 'fortune -u -e -c /storage/src/fortunes/ | tail +3 | cowsay -b -f dragon -W 80' &
