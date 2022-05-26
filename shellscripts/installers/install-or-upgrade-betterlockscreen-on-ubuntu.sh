4246b54302a554777876f9d3f2c6262f83be0ccf
tiago.paranhos <tiagoprn@gmail.com>
2022-05-26 16:58:24 -0300

    feat(installer): added a new installer for betterlockscreen.

---
+++ shellscripts/installers/install-or-upgrade-betterlockscreen-on-ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eou pipefail
+
+ROOT_PATH="/opt/betterlockscreen"
+DOWNLOAD_PATH="$ROOT_PATH/betterlockscreen-main"
+BIN_PATH=/usr/local/bin/
+CURRENT_USER=$USER
+
+sudo mkdir -p $ROOT_PATH && sudo chown -R "$CURRENT_USER" "$ROOT_PATH"
+
+if [ -d "$DOWNLOAD_PATH" ]; then
+	echo 'Removing previously built version...[WAIT]'
+	rm -fr $DOWNLOAD_PATH
+	echo 'Removing previously built version...[DONE]'
+fi
+
+echo 'Downloading betterlockscreen...'
+cd $ROOT_PATH && wget https://github.com/pavanjadhaw/betterlockscreen/archive/refs/heads/main.zip && unzip main.zip
+
+sudo chmod u+x $DOWNLOAD_PATH/betterlockscreen
+
+echo "Copying betterlockscreen to $BIN_PATH..."
+sudo cp $DOWNLOAD_PATH/betterlockscreen $BIN_PATH
+
+echo 'Copying betterlockscreen systemd unit file...'
+sudo cp $DOWNLOAD_PATH/system/betterlockscreen@.service /etc/systemd/system
+
+echo "Enabling betterlockscreen systemd service for user $CURRENT_USER..."
+sudo systemctl enable betterlockscreen@"$CURRENT_USER"
