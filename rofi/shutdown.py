b90c659006db448c4231d6824063ff0fc6d99d73
tiago.paranhos <tiagoprn@gmail.com>
2022-05-27 06:24:38 -0300

    feat(bin): adding/updating scripts.

--- rofi/shutdown.py
+++ rofi/shutdown.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 )
 
 ACTIONS = [
-    ('lock screen', 'lock_with_comic.sh',),
+    ('lock screen', 'lock-with-betterlockscreen.sh',),
     ('standby', 'xset dpms force standby',),
     ('switch user', 'dm-tool switch-to-greeter',),
     ('logoff', 'logoff.sh',),
