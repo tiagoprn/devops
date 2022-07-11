cf8437cf3a699b14b107de426c9e2cef31aa4ae5
tiago.paranhos <tiagoprn@gmail.com>
2022-07-11 06:51:51 -0300

    feat(python): upgraded pipx scripts.

--- python/pipx/desktop-utils/setup.sh
+++ python/pipx/desktop-utils/setup.sh
@@ -2,11 +2,10 @@
 
 set -eou pipefail
 
-# crazydiskmark:  to bookmark disks - equivalent to CrystalDiskMark on windows - needs python 3.8
 # howdoi <https://github.com/gleitz/howdoi>
 # termdown: countdown/stopwatch timer
 
-PACKAGES="pywal telegram-send crazydiskmark howdoi termdown"
+PACKAGES="pywal telegram-send howdoi termdown"
 for PACKAGE in $PACKAGES; do
     echo -e "Installing $PACKAGE..."
     pipx install "$PACKAGE" --include-deps
