456e6ce46155944065bf4f42ce50e56b8502df68
tiago.paranhos <tiagoprn@gmail.com>
2022-08-22 16:16:10 -0300

    feat(pipx): updated to install a more recent version of poetry.

--- python/pipx/core-utils/setup.sh
+++ python/pipx/core-utils/setup.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
 # jetty: alternative to jq
 
-PACKAGES="pre-commit ansible cookiecutter ipython s3cmd speedtest-cli tmuxp jetty"
+PACKAGES="pre-commit ansible cookiecutter ipython s3cmd speedtest-cli poetry tmuxp jetty"
 for PACKAGE in $PACKAGES; do
     echo -e "Installing $PACKAGE..."
     pipx install "$PACKAGE" --include-deps
