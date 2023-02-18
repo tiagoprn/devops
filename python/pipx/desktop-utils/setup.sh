03cf8569c1c0c7989a9e24f941324e13c0d34b9a
tiago.paranhos <tiagoprn@gmail.com>
2023-02-18 11:14:14 -0300

    feat(pipx): added bpytop to desktop utils.

--- python/pipx/desktop-utils/setup.sh
+++ python/pipx/desktop-utils/setup.sh
@@ -5,7 +5,7 @@ set -eou pipefail
 # howdoi <https://github.com/gleitz/howdoi>
 # termdown: countdown/stopwatch timer
 
-PACKAGES="pywal telegram-send howdoi termdown"
+PACKAGES="pywal telegram-send howdoi termdown bpytop"
 for PACKAGE in $PACKAGES; do
     echo -e "Installing $PACKAGE..."
     pipx install "$PACKAGE" --include-deps
