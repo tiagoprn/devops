eedf149e6dca5b3b4551f3a58616e966bdc5d6c4
tiago.paranhos <tiagoprn@gmail.com>
2025-01-16 15:32:48 -0300

    feat(shellscripts): updated kvm ssh script.

--- shellscripts/kvm/ssh.sh
+++ shellscripts/kvm/ssh.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 VM_IP=$(sudo virsh net-dhcp-leases vmnet | fzf --prompt 'Choose a running machine to ssh into (user=tds): ' | awk '{print $5}' | cut -d '/' -f 1)
-ssh tds@"$VM_IP"
+#
+# important: include the kvm machine IP on ~/.ssh/config with the IP and user, so that you can refer to it here below by its name
+ssh "$VM_IP"
